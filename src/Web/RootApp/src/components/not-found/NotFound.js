5f985a2504411533e0380a49c07c5563473c6b4c
Jesús Sánchez <j.sanchez-ext@lefebvre.es>
2019-08-26 09:22:12 +0200

    AB#136 Adding files needs to RootApp - clean code

---
+++ src/Web/RootApp/src/components/not-found/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './NotFound.css';
+
+export const NotFound = () => {
+  return (
+    <div className="d-flex w-100 h-100 flex-column justify-content-center align-items-center vertical-center">
+      <div className="h1">404 ERROR</div>
+      <div>Page not found.</div>
+    </div>
+  );
+};
