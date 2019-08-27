daac88e5740a18102d6a640ee0a2ceb7445468fc
freyes-lef <f.reyes-ext@lefebvre.es>
2019-08-27 12:24:21 +0200

    create base project from interactionEventLog in mongodb (provisional), create lexon.API with nugets libraries of catalog.API

--- src/BuildingBlocks/EventBus/IntegrationEventLogMongoDB/Services/IntegrationEventLogServiceMongoDB.cs
+++ src/BuildingBlocks/EventBus/IntegrationEventLogMongoDB/Services/IntegrationEventLogServiceMongoDB.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+using Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogEF;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogMongoDB.Services
@@ -8,6 +13,21 @@ namespace Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogMongoDB.
 
     public class IntegrationEventLogServiceMongoDB : IIntegrationEventLogServiceMongoDB
     {
+        private readonly IntegrationEventLogContextMongoDB _integrationEventLogContext;
+        private readonly List<Type> _eventTypes;
+
+        public IntegrationEventLogServiceMongoDB(
+            IOptions<IntegrationEventLogSettings> settings
+            )
+        {
+            _integrationEventLogContext = new IntegrationEventLogContextMongoDB(settings);
+
+            _eventTypes = Assembly.Load(Assembly.GetEntryAssembly().FullName)
+                .GetTypes()
+                .Where(t => t.Name.EndsWith(nameof(IntegrationEvent)))
+                .ToList();
+        }
+
         public Task MarkEventAsFailedAsync(Guid eventId)
         {
             throw new NotImplementedException();
@@ -23,9 +43,19 @@ namespace Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogMongoDB.
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId)
+        public async Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId)
         {
-            throw new NotImplementedException();
+            var builder = Builders<IntegrationEventLogEntry>.Filter;
+            var filter = Builders<IntegrationEventLogEntry>.Filter.Eq(u => u.State, EventStateEnum.NotPublished);
+            var filterEvent = Builders<IntegrationEventLogEntry>.Filter.In(u => u.EventTypeShortName, _eventTypes.Select(e => e.Name));
+            var filterTransaction = Builders<IntegrationEventLogEntry>.Filter.Eq(u => u.TransactionId, transactionId.ToString());
+            var finalFilter = builder.And(filter, filterEvent, filterTransaction);
+
+            var sort = Builders<IntegrationEventLogEntry>.Sort.Descending(u => u.CreationTime);
+            return await _integrationEventLogContext.IntegrationEventLogs
+                .Find(finalFilter)
+                .Sort(sort)
+                .ToListAsync();
         }
 
         public Task SaveEventAsync(IntegrationEvent @event)
