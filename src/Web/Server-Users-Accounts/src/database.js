25f3006d848abf6403c512d0d71c493fbc60ffef
Jesús Sánchez <j.sanchez-ext@lefebvre.es>
2019-08-30 13:52:42 +0200

    add new stringconnection to Server-Root-App

--- src/Web/Server-Users-Accounts/src/database.js
+++ src/Web/Server-Users-Accounts/src/database.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const URI = "mongodb://localhost/users-accounts";
+// const URI = "mongodb://localhost/users-accounts";
 
-mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
+// mongoose.connect(URI)
+//     .then(db => console.log('DB is connected'))
+//     .catch(err => console.log(err));
 
-module.exports = mongoose;
+// module.exports = mongoose;
 
 // const mongoose = require('mongoose');
 
@@ -24,3 +24,18 @@ module.exports = mongoose;
 
 // module.exports = mongoose;
 
+const mongoose = require('mongoose');
+
+const URI = "mongodb://mongolefebvre.documents.azure.com:10255/Lefebvre_services?ssl=true";
+
+mongoose.connect(URI, {
+    auth: {
+        user: 'mongolefebvre',
+        password: 'ir0EuCMlWqb6UxaeBRlRlqpPnEmvCstYqvwPN3AWAMSNjPnjv7Ul8X6CHKlmtuDYgTlrVkSPzhHb8jF79Hzeeg=='
+    },
+    useNewUrlParser: true
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
+
+module.exports = mongoose;
