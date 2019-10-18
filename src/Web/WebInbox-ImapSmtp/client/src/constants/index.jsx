cd18f1d8c8e2b2ef34ecd31de5020b0283be7aa6
Jesús Sánchez <j.sanchez-ext@lefebvre.es>
2019-10-18 11:10:11 +0200

    AB#148 Change routes for adjust ports and directions in url's and api's

--- src/Web/WebInbox-ImapSmtp/client/src/constants/index.jsx
+++ src/Web/WebInbox-ImapSmtp/client/src/constants/index.jsx
@@ -19,22 +19,22 @@ const dev = {
 };
 
 const prod = {
-  url: {
-    URL_UPDATE_DEFAULTACCOUNT:
-      "https://elefebvreoncontainers:8086/api/accounts/emailuseraccount/updatedefaultaccount",
-    URL_RESET_DEFAULTACCOUNT:
-      "https://elefebvreoncontainers:8086/api/accounts/emailuseraccount/resetdefaultaccountbyuser",
-    URL_DELETE_ACCOUNT:
-      "https://elefebvreoncontainers:8086/api/accounts/emailuseraccount/deleteaccountbyuserandprovider",
-    URL_SELECT_ACCOUNT: "https://elefebvreoncontainers:8082",
-    URL_GET_ACCOUNTS:
-      "https://elefebvreoncontainers:8086/api/accounts/emailuseraccount",
-    URL_MF_GOOGLE: "https://elefebvreoncontainers:8083/user",
-    URL_MF_OUTLOOK:
-      "https://lefebvre-multichannel-office365.azurewebsites.net/user",
-    URL_MF_IMAP: "https://elefebvreoncontainers:8081/user",
-    URL_MF_LEXON: "https://elefebvreoncontainers:8085/static/js/main.js"
-  }
+    url: {
+        URL_UPDATE_DEFAULTACCOUNT:
+            "http://elefebvreoncontainers:8083/api/accounts/emailuseraccount/updatedefaultaccount",
+        URL_RESET_DEFAULTACCOUNT:
+            "http://elefebvreoncontainers:8083/api/accounts/emailuseraccount/resetdefaultaccountbyuser",
+        URL_DELETE_ACCOUNT:
+            "http://elefebvreoncontainers:8083/api/accounts/emailuseraccount/deleteaccountbyuserandprovider",
+        URL_SELECT_ACCOUNT: "https://elefebvreoncontainers:8087",
+        URL_GET_ACCOUNTS:
+            "http://elefebvreoncontainers:8083/api/accounts/emailuseraccount",
+        URL_MF_GOOGLE: "http://elefebvreoncontainers:8088/user",
+        URL_MF_OUTLOOK:
+            "https://lefebvre-multichannel-office365.azurewebsites.net/user",
+        URL_MF_IMAP: "http://elefebvreoncontainers:8080/user",
+        URL_MF_LEXON: "http://elefebvreoncontainers:8090/static/js/main.js"
+    }
 };
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
