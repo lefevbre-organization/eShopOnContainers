2ff202f5dd66dc0b7726fc42a39d465dc7ce45b1
Jesús Sánchez <j.sanchez-ext@lefebvre.es>
2019-10-21 11:22:46 +0200

    AB#148 Resolved errors to up prod

--- src/Web/WebInbox-Graph/src/constants/index.jsx
+++ src/Web/WebInbox-Graph/src/constants/index.jsx
@@ -6,17 +6,36 @@ export const AUTH_FAIL = "AUTH_FAIL";
 export const AUTH_SIGNED_OUT = "AUTH_SIGNED_OUT";
 export const PROVIDER = "OUTLOOK";
 
+// const dev = {
+//   url: {
+//     URL_UPDATE_DEFAULTACCOUNT:
+//       "http://localhost:60980/api/v1/accounts/emailuseraccount/updatedefaultaccount",
+//     URL_RESET_DEFAULTACCOUNT:
+//       "http://localhost:60980/api/v1/accounts/emailuseraccount/resetdefaultaccountbyuser",
+//     URL_DELETE_ACCOUNT:
+//       "http://localhost:60980/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider",
+//     URL_SELECT_ACCOUNT: "http://localhost:3010",
+//     URL_GET_ACCOUNTS:
+//       "http://localhost:60980/api/v1/accounts/emailuseraccount/getbyuser",
+//     URL_MF_GOOGLE: "http://localhost:3001/user",
+//     URL_MF_OUTLOOK: "http://localhost:3002/user",
+//     URL_MF_IMAP: "http://localhost:9000/user",
+//     URL_MF_LEXON: "http://localhost:3004/static/js/main.js"
+//   }
+// };
+
 const dev = {
   url: {
     URL_UPDATE_DEFAULTACCOUNT:
-      "http://localhost:60980/api/v1/accounts/emailuseraccount/updatedefaultaccount",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/updatedefaultaccount",
     URL_RESET_DEFAULTACCOUNT:
-      "http://localhost:60980/api/v1/accounts/emailuseraccount/resetdefaultaccountbyuser",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/resetdefaultaccountbyuser",
     URL_DELETE_ACCOUNT:
-      "http://localhost:60980/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider",
-    URL_SELECT_ACCOUNT: "http://localhost:3010",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider",
+    URL_SELECT_ACCOUNT: "http://lefebvre.westeurope.cloudapp.azure.com:8087",
     URL_GET_ACCOUNTS:
-      "http://localhost:60980/api/v1/accounts/emailuseraccount/getbyuser",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/getbyuser",
+
     URL_MF_GOOGLE: "http://localhost:3001/user",
     URL_MF_OUTLOOK: "http://localhost:3002/user",
     URL_MF_IMAP: "http://localhost:9000/user",
@@ -27,19 +46,20 @@ const dev = {
 const prod = {
   url: {
     URL_UPDATE_DEFAULTACCOUNT:
-        "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/accounts/emailuseraccount/updatedefaultaccount",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/updatedefaultaccount",
     URL_RESET_DEFAULTACCOUNT:
-        "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/accounts/emailuseraccount/resetdefaultaccountbyuser",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/resetdefaultaccountbyuser",
     URL_DELETE_ACCOUNT:
-        "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/accounts/emailuseraccount/deleteaccountbyuserandprovider",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider",
     URL_SELECT_ACCOUNT: "http://lefebvre.westeurope.cloudapp.azure.com:8087",
     URL_GET_ACCOUNTS:
-        "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/accounts/emailuseraccount",
+      "http://lefebvre.westeurope.cloudapp.azure.com:8083/api/v1/accounts/emailuseraccount/getbyuser",
     URL_MF_GOOGLE: "http://lefebvre.westeurope.cloudapp.azure.com:8088/user",
     URL_MF_OUTLOOK:
-        "https://lefebvre-multichannel-office365.azurewebsites.net/user",
+      "https://lefebvre-multichannel-office365.azurewebsites.net/user",
     URL_MF_IMAP: "http://lefebvre.westeurope.cloudapp.azure.com:8080/user",
-    URL_MF_LEXON: "http://lefebvre.westeurope.cloudapp.azure.com:8090/static/js/main.js"
+    URL_MF_LEXON:
+      "https://lefebvre-multichannel-lex-on.azurewebsites.net"
   }
 };
 
