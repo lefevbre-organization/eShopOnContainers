c6cb996df40ca249d59b9de6b22104a8607bfb1e
Alberto VALVERDE <a.valverde-ext@lefebvre.es>
2019-10-22 20:02:18 +0200

    adding WebNetwork

---
+++ src/Web/WebNetwork/output/nodes.js
@@ -0,0 +1,13 @@
+var nodes = 
+[{"id":0,"label":"Account Gateway","group":"gw"},
+{"id":1,"label":"LEX-ON Gateway","group":"gw"},
+{"id":2,"label":"Account Service","group":"service"},
+{"id":3,"label":"LEX-ON Service","group":"service"},
+{"id":4,"label":"Google app","group":"app"},
+{"id":5,"label":"Outlook app","group":"app"},
+{"id":6,"label":"IMAP-SMTP app","group":"app"},
+{"id":7,"label":"LEX-ON connector","group":"mf"},
+{"id":8,"label":"Main Root","group":"root"},
+{"id":9,"label":"RabbitMQ Server","group":"rabbitmq"},
+{"id":10,"label":"MongoDB Server","group":"mongo"}
+]
