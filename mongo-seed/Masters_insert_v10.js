191744d3100a4b61abba3678f72d9b3c7041868d
freyes-lef <f.reyes-ext@lefebvre.es>
2019-10-25 15:25:03 +0200

    add cert to image

---
+++ mongo-seed/Masters_insert_v10.js
@@ -0,0 +1,65 @@
+db.Masters.insert([
+    {   
+        "type": "Entities",
+        "timestamp": 1569837286,
+        "Version": 9,
+        "list": [
+            {
+                "idEntity": 1,
+                "name": "Expedientes"
+            },
+            {
+                "idEntity": 2,
+                "name": "Clientes"
+            },
+            {
+                "idEntity": 3,
+                "name": "Contrarios"
+            },
+            {
+                "idEntity": 4,
+                "name": "Proveedores"
+            },
+            {
+                "idEntity": 5,
+                "name": "Abogados propios"
+            },
+            {
+                "idEntity": 6,
+                "name": "Abogados contrarios"
+            },
+            {
+                "idEntity": 7,
+                "name": "Procuradores propios"
+            },
+            {
+                "idEntity": 8,
+                "name": "Procuradores contrarios"
+            },
+            {
+                "idEntity": 9,
+                "name": "Notarios"
+            },
+            {
+                "idEntity": 10,
+                "name": "Juzgados"
+            },
+            {
+                "idEntity": 11,
+                "name": "Aseguradoras"
+            },
+            {
+                "idEntity": 12,
+                "name": "Otros"
+            },
+            {
+                "idEntity": 13,
+                "name": "Carpetas"
+            },
+            {
+                "idEntity": 14,
+                "name": "Documentos"
+            }
+        ]
+    }
+])
