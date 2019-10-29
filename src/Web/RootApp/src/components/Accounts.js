45a4ed8f2e87808bdb1f824e9e3c8a1e0b07fd14
Jesus SANCHEZ <j.sanchez-ext@lefebvre.es>
2019-10-29 15:24:40 +0100

    AB#222 Add styles to RootApp

--- src/Web/RootApp/src/components/Accounts.js
+++ src/Web/RootApp/src/components/Accounts.js
@@ -1,14 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ProviderInbox from "./ProviderInbox";
-import i18n from "i18next";
 
 class Accounts extends Component {
-  // renderWithOutAccounts() {
-  //   if (this.props.accounts == null || this.props.accounts.length === 0) {
-  //     return <span>{i18n.t("accounts.nothing-user-email-added")}</span>;
-  //   }
-  // }
-
   render() {
     const { userId, accounts } = this.props;
 
@@ -17,30 +10,23 @@ class Accounts extends Component {
     }
 
     return (
-      <div className="row content bg-grey">
-        <div className="col-12 mt-2">
-          <span className="asociadas"> {i18n.t("page-goto.my-accounts")}</span>
-        </div>
-        <div className="col-12 p-0">
-          <ul>
-            {accounts.map(el => {
-              return (
-                <li key={el.provider}>
-                  <ProviderInbox
-                    userId={userId}
-                    provider={el.provider}
-                    email={el.email}
-                    removeAccount={this.props.removeAccount}
-                    toggleConfirmRemoveAccount={
-                      this.props.toggleConfirmRemoveAccount
-                    }
-                  />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+      <Fragment>
+        {accounts.map(el => {
+          return (
+            <li key={el.provider}>
+              <ProviderInbox
+                userId={userId}
+                provider={el.provider}
+                email={el.email}
+                removeAccount={this.props.removeAccount}
+                toggleConfirmRemoveAccount={
+                  this.props.toggleConfirmRemoveAccount
+                }
+              />
+            </li>
+          );
+        })}
+      </Fragment>
     );
   }
 }
