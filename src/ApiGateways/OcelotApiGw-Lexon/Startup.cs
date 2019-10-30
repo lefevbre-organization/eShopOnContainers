7de510a277dd6d50d869b876f15c547e34e93032
freyes-lef <f.reyes-ext@lefebvre.es>
2019-10-30 18:18:40 +0100

    add account and gateway account to compose

--- src/ApiGateways/OcelotApiGw-Lexon/Startup.cs
+++ src/ApiGateways/OcelotApiGw-Lexon/Startup.cs
@@ -89,7 +89,6 @@ namespace OcelotApiGw_Lexon
             }
             else
             {
-                app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
