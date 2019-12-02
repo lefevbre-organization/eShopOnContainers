6a9b0060a42d20ab5205c93650a9d35be3a5873a
Alberto VALVERDE <a.valverde-ext@lefebvre.es>
2019-12-02 12:50:19 +0100

    adding persistent volumes

--- k8s-LEF/helm-pre/deploy-traefik-rules.ps1
+++
@@ -1,7 +0,0 @@
-kubectl apply -f .\traefik\webimap-traefik-rules.yaml
-kubectl apply -f .\traefik\accountapigw-traefik-rules.yaml
-kubectl apply -f .\traefik\lexonapigw-traefik-rules.yaml
-kubectl apply -f .\traefik\webgoogle-traefik-rules.yaml
-kubectl apply -f .\traefik\webgraph-traefik-rules.yaml
-kubectl apply -f .\traefik\weblexon-traefik-rules.yaml
-kubectl apply -f .\traefik\webportal-traefik-rules.yaml
