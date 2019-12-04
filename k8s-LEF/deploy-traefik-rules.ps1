e9242043b7261b1a73d9c79815776f6354718867
Alberto VALVERDE <a.valverde-ext@lefebvre.es>
2019-12-04 11:10:04 +0100

    Adding mongo version 4 and rabbitmq persistence

--- k8s-LEF/deploy-traefik-rules.ps1
+++ k8s-LEF/deploy-traefik-rules.ps1
@@ -5,3 +5,4 @@ kubectl apply -f .\traefik\webgoogle-traefik-rules.yaml
 kubectl apply -f .\traefik\webgraph-traefik-rules.yaml
 kubectl apply -f .\traefik\weblexon-traefik-rules.yaml
 kubectl apply -f .\traefik\webportal-traefik-rules.yaml
+kubectl apply -f .\traefik\elefebvre-rabbitmq-cluster-persistente.yaml
