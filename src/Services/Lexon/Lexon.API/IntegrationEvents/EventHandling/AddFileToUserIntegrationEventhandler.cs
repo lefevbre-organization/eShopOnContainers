7d70188d31253fa68fa7bc34593a1701b31fc7e0
freyes-lef <f.reyes-ext@lefebvre.es>
2020-01-13 13:27:18 +0100

    api changues with classifications changues of 3.0 version

--- src/Services/Lexon/Lexon.API/IntegrationEvents/EventHandling/AddFileToUserIntegrationEventhandler.cs
+++ src/Services/Lexon/Lexon.API/IntegrationEvents/EventHandling/AddFileToUserIntegrationEventhandler.cs
@@ -6,23 +6,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
 
 namespace Lexon.API.IntegrationsEvents.EventHandling
 {
     public class AddFileToUserIntegrationEventHandler: IIntegrationEventHandler<AddFileToUserIntegrationEvent>
     {
         private readonly IUsersRepository _repository;
+        private readonly ILogger<AddFileToUserIntegrationEventHandler> _logger;
 
         public AddFileToUserIntegrationEventHandler(
-            IUsersRepository repository
-            )
+            IUsersRepository repository,
+            ILogger<AddFileToUserIntegrationEventHandler> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task Handle(AddFileToUserIntegrationEvent @event)
         {
-            await _repository.AddFileToListAsync(@event.UserId, @event.CompanyId, @event.FileId, @event.FileName, @event.FileDescription);
+            await _repository.AddFileToListAsync(@event.UserId, @event.Bbdd, @event.FileId, @event.FileName, @event.FileDescription);
+            using (LogContext.PushProperty("IntegrationEventContext", $"{@event.Id}-{Program.AppName}"))
+            {
+                _logger.LogInformation("----- Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})", @event.Id, Program.AppName, @event);
+                await _repository.AddFileToListAsync(@event.UserId, @event.Bbdd, @event.FileId, @event.FileName, @event.FileDescription);
+                //await _repository.DeleteBasketAsync(@event.UserId.ToString());
+            }
         }
     }
 }
---
+++ src/Services/Lexon/Lexon.MySql/ViewModel/MailInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Lexon.MySql.Model
+{
+    public class MailInfo
+    {
+        public string Uid { get; set; }
+        public string Subject { get; set; }
+        public string Date { get; set; }
+    }
+}
