6592b4e9ab5ef48804b4a804f4ecfaea455b0b43
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-01-31 14:54:46 +0100

    AB#329. Portal send account to imap client

--- src/Web/WebImapClient/Client/client/src/routes/routes.jsx
+++ src/Web/WebImapClient/Client/client/src/routes/routes.jsx
@@ -14,9 +14,14 @@ class Routes extends Component {
       <Router basename="/" history={history}>
         <Switch>
           <Route exact path="/user/:idUser" component={UserLexon} />
-          <Route exact path="/user/:idUser/casefile/:idCaseFile/bbdd/:bbdd/company/:idCompany" component={UserLexon}/>
           <Route exact path="/user/:idUser/bbdd/:bbdd" component={UserLexon} />
           <Route exact path="/user/:idUser/folder/:idFolder/message/:idMessage/casefile/:idCaseFile" component={UserLexon}/>
+          <Route exact path="/user/:idUser/account/:account/" component={UserLexon} />
+          <Route
+            exact
+            path="/user/:idUser/casefile/:idCaseFile/bbdd/:bbdd/company/:idCompany"
+            component={UserLexon}
+          />
           <Route exact path="/login" render={() => <Login />} />
           <PrivateRoute exact path="/" component={App} />
           <PrivateRoute exact path="/settings" component={Settings} />
