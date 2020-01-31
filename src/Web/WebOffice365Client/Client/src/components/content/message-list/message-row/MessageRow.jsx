a232fbba6e210952a61470d65449070c9986e1b8
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-01-31 11:28:18 +0100

    AB#357 adding moment.js "es" configuration to fix the bug

--- src/Web/WebOffice365Client/Client/src/components/content/message-list/message-row/MessageRow.jsx
+++ src/Web/WebOffice365Client/Client/src/components/content/message-list/message-row/MessageRow.jsx
@@ -29,8 +29,21 @@ export class MessageItem extends PureComponent {
     return nameEmail.name;
   }
 
-  getFormattedDate(date, fallbackDateObj) {
+    getFormattedDate(date, fallbackDateObj) {
+
     let messageDate = moment(date);
+    var locale = window.navigator.userLanguage || window.navigator.language;
+        if(locale === "es-ES")
+        {
+            moment.lang('es', {
+                months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+                monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+                weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+                weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+                weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+            });
+        }  
+
     if (!messageDate.isValid()) {
       messageDate = moment(fallbackDateObj.parserFn(fallbackDateObj.date));
     }
@@ -46,7 +59,8 @@ export class MessageItem extends PureComponent {
       } else {
         formattedDate = messageDate.format("MMM D");
       }
-    }
+      }
+    
     return formattedDate;
   }
 
