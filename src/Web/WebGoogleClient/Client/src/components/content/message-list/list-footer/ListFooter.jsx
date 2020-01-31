40004aa85fdf484d76a18bcc8b50a6773e557987
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-01-31 10:05:55 +0100

    AB#358 + AB#344 Adfing new helm for PRO and fixing traslation bug in totla messages

--- src/Web/WebGoogleClient/Client/src/components/content/message-list/list-footer/ListFooter.jsx
+++ src/Web/WebGoogleClient/Client/src/components/content/message-list/list-footer/ListFooter.jsx
@@ -1,12 +1,19 @@
 import React, { PureComponent } from "react";
+import { withTranslation } from "react-i18next";
 import './ListFooter.scss';
 
 export class Footer extends PureComponent {
-  render() {
+    constructor(props) {
+        super(props);
+       
+    }
+    render() {
+    const { t } = this.props;
+
     const { messagesTotal } = this.props;
     let totalLabel = '';
     if (messagesTotal > 0) {
-      totalLabel = `${messagesTotal.toLocaleString()} messages`;
+        totalLabel = `${messagesTotal.toLocaleString()}` + " "  + `${ t("message-footer.messages") }`;
     }
 
     return (
@@ -20,4 +27,4 @@ export class Footer extends PureComponent {
   }
 }
 
-export default Footer;
+export default withTranslation() (Footer);
