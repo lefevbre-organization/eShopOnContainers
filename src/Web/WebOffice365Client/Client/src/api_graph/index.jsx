2a61d04efbf3572905e2301049b7f02044d4fdaf
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-02-05 18:05:26 +0100

    AB#379

--- src/Web/WebOffice365Client/Client/src/api_graph/index.jsx
+++ src/Web/WebOffice365Client/Client/src/api_graph/index.jsx
@@ -547,19 +547,20 @@ export const uploadFileWithUploadSession = async(emailId, file, content) => {
 export const getContacts = () =>
 
     new Promise(async (resolve, reject) => {
-        const accessToken = await getAccessTokenSilent();
-        const client = getAuthenticatedClient(accessToken);
-        client
-            .api(`me/contacts`)
-            .get()
-            .then(response => {
-                let arr = response.value;
-                let contacts = [];
-                arr.map(function (item) {
-                    contacts.push(item.emailAddresses[0].address);
-                })
-                resolve(contacts);
-            });
+      resolve(["aaaa@aa.com", "bbbb@bb.com", "ccc@cc.com", "dddd@dd.com"])
+        // const accessToken = await getAccessTokenSilent();
+        // const client = getAuthenticatedClient(accessToken);
+        // client
+        //     .api(`me/contacts`)
+        //     .get()
+        //     .then(response => {
+        //         let arr = response.value;
+        //         let contacts = [];
+        //         arr.map(function (item) {
+        //             contacts.push(item.emailAddresses[0].address);
+        //         })
+        //         resolve(contacts);
+        //     });
 
     });
 
