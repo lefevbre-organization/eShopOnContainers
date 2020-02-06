632cf4d29efe8b23655292883d89b434bbda55e4
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-02-06 11:01:40 +0100

    adding new config for kubernetes and gitignore

--- src/Web/WebGoogleClient/Client/config.jsx
+++
@@ -1,5 +0,0 @@
-export const config = {
-    CLIENT_ID: 'CM2KN2gjeaqdy6L7aiZLJkJP', // Must be taken from Google API Console (https://console.developers.google.com/)
-    API_KEY: '539239646995-24dmrd01c8vhlbumkg1dlfsfijog08dp.apps.googleusercontent.com', // https://developers.google.com/maps/documentation/javascript/get-api-key
-    PUBLIC_URL: 'http://localhost:3000'
-} 
