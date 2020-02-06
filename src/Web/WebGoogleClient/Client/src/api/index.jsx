733fd186e49caed1a381af1ae98fb266f13f5ab3
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-02-06 13:11:55 +0100

    AB#354 adding google auth access to read contacts

--- src/Web/WebGoogleClient/Client/src/api/index.jsx
+++ src/Web/WebGoogleClient/Client/src/api/index.jsx
@@ -107,7 +107,6 @@ const getMessageRawList = ({ labelIds, maxResults, pageToken, q = "" }) =>
 export const getContacts = () =>
     
     new Promise((resolve, reject) => {
-<<<<<<< HEAD
        // resolve(["aaaa@aa.com", "bbbb@bb.com", "ccc@cc.com", "dddd@dd.com"])
          window.gapi.client.people.people.connections.list({
              'resourceName': 'people/me',
@@ -121,24 +120,7 @@ export const getContacts = () =>
                      contacts.push(item.names[0].givenName);
                  })
                  resolve(contacts);
-             });
-=======
-        resolve(["aaaa@aa.com", "bbbb@bb.com", "ccc@cc.com", "dddd@dd.com"])
-        // window.gapi.client.people.people.connections.list({
-        //     'resourceName': 'people/me',
-        //     'pageSize': 100,
-        //     'personFields': 'names'
-        // })
-        //     .then(response => {
-        //         let arr = response.result.connections;
-        //         let contacts = [];
-        //         arr.map(function (item) {
-        //             contacts.push(item.names[0].givenName);
-        //         })
-        //         resolve(contacts);
-        //     });
->>>>>>> AB#380
-            
+             });            
     });
 
 
