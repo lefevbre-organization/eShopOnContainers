dac940ae10cd268087c515d347c9962a925d2556
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-02-17 12:18:35 +0100

    AB#391

--- src/Web/WebImapClient/Client/client/assets/config.js
+++ src/Web/WebImapClient/Client/client/assets/config.js
@@ -1,13 +1,13 @@
 window.REACT_APP_ENVIRONMENT = "LOCAL";
 window.URL_DELETE_ACCOUNT =
-  "https://now-apigwacc.lefebvre.es/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider";
+  "https://lexbox-apigwacc.lefebvre.es/api/v1/accounts/emailuseraccount/deleteaccountbyuserandprovider";
 window.URL_SELECT_ACCOUNT = "http://localhost:3010";
 window.URL_GET_ACCOUNTS =
-  "https://now-apigwacc.lefebvre.es/api/v2/accounts/usermail";
+  "https://lexbox-apigwacc.lefebvre.es/api/v2/accounts/usermail";
 window.URL_MF_GOOGLE = "http://localhost:3001/user";
 window.URL_MF_OUTLOOK = "http://localhost:3002/user";
 window.URL_MF_IMAP = "http://localhost:9000/user";
 window.URL_MF_LEXON = "http://localhost:3004/static/js/main.js";
 //window.URL_MF_LEXON = "https://now-weblexon.lefebvre.es/static/js/main.js";
-window.API_GATEWAY = "https://now-apigwlex.lefebvre.es";
-window.API_ACC_GATEWAY = "https://now-apigwacc.lefebvre.es";
+window.API_GATEWAY = "https://lexbox-apigwlex.lefebvre.es";
+window.API_ACC_GATEWAY = "https://lexbox-apigwacc.lefebvre.es";
