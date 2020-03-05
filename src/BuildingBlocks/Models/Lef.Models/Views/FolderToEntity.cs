da1101f1b6960555ccdd2341a4d92c6ffcfdb446
freyes-lef <f.reyes-ext@lefebvre.es>
2020-03-05 16:01:35 +0100

    refactorice methods

---
+++ src/BuildingBlocks/Models/Lef.Models/Views/FolderToEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.eShopOnContainers.BuildingBlocks.Lefebvre.Models
+{
+    public class FolderToEntity: EntitySearchById
+    {
+        /// <summary>
+        /// Nombre de la carpeta, si no se envia, usara el nombre de la entidad en base a su id
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Carpeta de la que colgará la carpeta creada si se precisa
+        /// </summary>
+        public int? IdParent { get; set; }
+    }
+}
