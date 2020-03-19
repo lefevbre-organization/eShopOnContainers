c836aa79ae7a7c7cb60ea0ec832e411dd7a24050
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-03-19 14:25:42 +0100

    AB#238

--- src/Web/WebLexonClient/Client/src/components/select-action/select-action-tab/select-action-tab.js
+++ src/Web/WebLexonClient/Client/src/components/select-action/select-action-tab/select-action-tab.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import "./select-action-tab.css";
-import PropTypes from "prop-types";
-import { Tabs, Tab } from "react-bootstrap";
-import i18n from "i18next";
+import React, { Component } from 'react';
+import './select-action-tab.css';
+import PropTypes from 'prop-types';
+import { Tabs, Tab } from 'react-bootstrap';
+import i18n from 'i18next';
 
-import TabClassify from "../tab-classify/tab-classify";
-import TabDocument from "../tab-document/tab-document";
-import TabConnectMessage from "../tab-connect-message/tab-connect-message";
+import TabClassify from '../tab-classify/tab-classify';
+import TabDocument from '../tab-document/tab-document';
+import TabConnectMessage from '../tab-connect-message/tab-connect-message';
 
 class SelectActionTab extends Component {
   render() {
@@ -14,18 +14,22 @@ class SelectActionTab extends Component {
 
     return (
       <Tabs
-        defaultActiveKey="connect"
-        id="uncontrolled-tab-example"
-        bsPrefix="menu-lexon-actions"
-      >
-        {/* <Tab eventKey="classify" title="Clasificar">
-          <TabClassify
+        defaultActiveKey='connect'
+        id='uncontrolled-tab-example'
+        bsPrefix='menu-lexon-actions'>
+        <Tab
+          eventKey='connect'
+          title={i18n.t('select-action.connect-messages')}>
+          <TabConnectMessage
             user={user}
             toggleNotification={toggleNotification}
           />
-        </Tab> */}
-        <Tab eventKey="connect" title={i18n.t("select-action.connect-messages")}>
-          <TabConnectMessage user={user} toggleNotification={toggleNotification}/>
+        </Tab>
+        <Tab eventKey='attach' title={i18n.t('select-action.attach-documents')}>
+          <TabConnectMessage
+            user={user}
+            toggleNotification={toggleNotification}
+          />
         </Tab>
 
         {/* <Tab eventKey="classify" title="Clasificar">
