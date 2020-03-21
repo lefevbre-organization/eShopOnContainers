528c477110e2642dd70fb95117b7755430887376
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-03-21 14:24:12 +0100

    updating google pipeline

--- src/Web/WebGoogleClient/Client/src/components/header/Header.jsx
+++ src/Web/WebGoogleClient/Client/src/components/header/Header.jsx
@@ -128,7 +128,7 @@ export class Header extends PureComponent {
             onSignout={this.props.onSignout}
             onSignoutDisconnect={this.props.onSignoutDisconnect}
           />
-            Testing 1
+            Testing 2
           <div>
             <Nav className='ml-auto' navbar>
               <NavItem className='d-md-down-none'>
