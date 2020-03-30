0986e1b43237881ef48c760b245ae09643305315
freyes-lef <f.reyes-ext@lefebvre.es>
2020-03-30 18:13:12 +0200

    logs to test files calls  and configure settings to development

--- src/Services/Lexon/Lexon.API/Infrastructure/Services/UsersService.cs
+++ src/Services/Lexon/Lexon.API/Infrastructure/Services/UsersService.cs
@@ -313,8 +313,10 @@ namespace Lexon.Infrastructure.Services
                 byte[] buffer = Encoding.UTF8.GetBytes(json);
                 var dataparameters = Convert.ToBase64String(buffer);
                 var url = $"{_settings.Value.LexonFilesUrl}?option=com_lexon&task=hook.receive&type=repository&data={dataparameters}";
+                WriteLine($"Se hace llamada a {url} a las {DateTime.Now}");
                 using (var response = await _clientFiles.GetAsync(url))
                 {
+                    WriteLine($"Se recibe contestación {DateTime.Now}");
 
                     if (response.IsSuccessStatusCode)
                     {
@@ -336,6 +338,7 @@ namespace Lexon.Infrastructure.Services
                 TraceOutputMessage(result.errors, $"Error al guardar el archivo {fileMail.idEntity}, -> {ex.Message}", "599");
             }
 
+            WriteLine($"Salimos de FileGetAsync a las {DateTime.Now}");
             return result;
         }
 
@@ -352,8 +355,11 @@ namespace Lexon.Infrastructure.Services
 
                 SerializeObjectToPut(fileMail.ContentFile, $"?option=com_lexon&task=hook.receive&type=repository&data={dataparameters}", out string url, out ByteArrayContent data);
 
+                WriteLine($"Se hace llamada a {url} a las {DateTime.Now}");
                 using (var response = await _clientFiles.PutAsync(url, data))
                 {
+                    WriteLine($"Se recibe contestación {DateTime.Now}");
+
                     var responseText = await response.Content.ReadAsStringAsync();
                     if (response.IsSuccessStatusCode)
                     {
@@ -371,6 +377,7 @@ namespace Lexon.Infrastructure.Services
                 //TraceMessage(result.errors, ex);
                 TraceOutputMessage(result.errors, $"Error al guardar el archivo {fileMail.Name}, -> {ex.Message}", "598");
             }
+            WriteLine($"Salimos de FilePostAsync a las {DateTime.Now}");
 
             return result;
         }
