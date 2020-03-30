96ce096217d1900e36474e67576096269514b9fd
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-03-30 15:37:00 +0200

    AB#220 adding syncfusion controls in google app

---
+++ src/Web/WebGoogleClient/Client/src/AppContainerCalendar.jsx
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import Main from './components/calendar/calendar';
+import Login from './components/login/Login';
+import Authenticating from './components/authenticating/Authenticating';
+import { connect } from 'react-redux';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { signIn, checkSignInStatus } from './api/authentication';
+import { mountScripts } from './api/scripts';
+import {
+  SIGNED_OUT,
+  AUTH_SUCCESS,
+  AUTH_FAIL,
+  AUTH_IN_PROGRESS
+} from './constants';
+import { getStateStorage } from './localstorage';
+import ACTIONS from './actions/lexon';
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signInStatus: SIGNED_OUT,
+      googleUser: undefined,
+      openEmail: undefined
+    };
+
+    this.init = this.init.bind(this);
+    this.initClient = this.initClient.bind(this);
+    this.onSignout = this.onSignout.bind(this);
+    this.onSignoutDisconnect = this.onSignoutDisconnect.bind(this);
+    this.onSignInSuccess = this.onSignInSuccess.bind(this);
+    this.onSignIn = this.onSignIn.bind(this);
+  }
+
+  componentDidMount() {
+    const user = this.props.match.params.idUser;
+    const stateStorageLexon = getStateStorage();
+    if (!user && stateStorageLexon) {
+      const userLexon = stateStorageLexon.lexon;
+      if (userLexon && userLexon.user) {
+        this.props.setUser(userLexon.user);
+        this.props.setCaseFile({
+          casefile: userLexon.idCaseFile,
+          bbdd: userLexon.bbdd,
+          company: userLexon.idCompany
+        });
+      }
+    }
+
+    const { isNewAccount } = this.props.lexon;
+    if (!isNewAccount) {
+      mountScripts().then(this.init);
+    }
+
+    if (this.props.match.params.idMail) {
+      this.setState({ openEmail: this.props.match.params.idMail });
+    } else {
+      this.props.location.pathname = '/inbox';
+    }
+  }
+
+  init() {
+    window.gapi.load('client:auth2', this.initClient);
+  }
+
+  initClient() {
+    checkSignInStatus()
+      .then(this.onSignInSuccess)
+      .catch(_ => {
+        this.setState({
+          signInStatus: AUTH_FAIL
+        });
+      });
+  }
+
+  onSignout() {
+    this.props.signOut();
+  }
+
+  onSignoutDisconnect() {
+    this.props.signOutDisconnect();
+  }
+
+  onSignIn() {
+    signIn().then(this.onSignInSuccess);
+  }
+
+  onSignInSuccess(googleUser) {
+    this.setState({
+      signInStatus: AUTH_SUCCESS,
+      googleUser: googleUser,
+      openEmail: this.props.match.params.idMail
+    });
+
+    this.props.setAccount(googleUser.Rt.Au);
+  }
+
+  renderView() {
+    const { signInStatus, openEmail } = this.state;
+
+    if (signInStatus === AUTH_SUCCESS) {
+      return <Main googleUser={this.state.googleUser} idEmail={openEmail} />;
+    } else if (signInStatus === AUTH_IN_PROGRESS) {
+      return <Authenticating />;
+    } else {
+      return <Login lexon={this.props.lexon} onSignIn={this.onSignIn} />;
+    }
+  }
+
+  render() {
+    return <React.Fragment>{this.renderView()}</React.Fragment>;
+  }
+}
+
+const mapStateToProps = state => ({
+  lexon: state.lexon
+});
+
+const mapDispatchToProps = dispatch => ({
+  setUser: user => dispatch(ACTIONS.setUser(user)),
+  setCaseFile: casefile => dispatch(ACTIONS.setCaseFile(casefile)),
+  setAccount: account => dispatch(ACTIONS.setAccount(account))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
---
+++ src/Web/WebGoogleClient/Client/src/components/calendar/sidebar/Sidebar.jsx
@@ -0,0 +1,197 @@
+import React, { PureComponent } from "react";
+import { withTranslation } from "react-i18next";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import groupBy from "lodash/groupBy";
+import sortBy from "lodash/sortBy";
+import {
+  faInbox,
+  faEnvelopeSquare,
+  faTrashAlt,
+  faFolderOpen,
+  faExclamationTriangle,
+  faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
+import LabelItem from "./LabelItem";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { CalendarComponent, ChangedEventArgs } from '@syncfusion/ej2-react-calendars';
+
+import "./sidebar.scss";
+
+export class Sidebar extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLabel: props.pathname,
+      leftSideBarOpen: true
+    };
+
+    //this.renderLabels = this.renderLabels.bind(this);
+    this.navigateToList = this.navigateToList.bind(this);
+    this.composeClick = this.composeClick.bind(this);
+    this.sidebarAction = this.sidebarAction.bind(this);
+  }
+
+  componentDidMount() {
+    //this.props.getLabelList(); //.then(labels => {});
+    /*  */
+    //this.props.getLabelMessages();
+  }
+
+  composeClick() {
+    this.props.history.push("/compose");
+  }
+
+  navigateToList(evt, labelId) {
+    const label = this.props.labelsResult.labels.find(el => el.id === labelId);
+    this.props.onLabelClick(label || { id: "" });
+  }
+
+  sidebarAction() {
+    this.props.onSidebarCloseClick(this.state.leftSideBarOpen);
+  }
+
+  renderItems(labelList) {
+    if (labelList.length === 0) {
+      return <div />;
+    }
+
+    const labels = labelList.reduce((acc, el) => {
+      acc.push(el);
+      return acc;
+    }, []);
+
+    const labelGroups = groupBy(labels, "type");
+    var visibleLabels = [];
+    var sortedLabels = [];
+
+    if (labelGroups.user != null) {
+      visibleLabels = labelGroups.user.filter(
+        el =>
+          //el.labelListVisibility === "labelShow" ||
+          //el.labelListVisibility === "labelShowIfUnread" ||
+          !el.labelListVisibility || true
+      );
+      sortedLabels = sortBy(visibleLabels, "name");
+    }
+
+    return (
+      <React.Fragment>
+        {this.renderFolders(labelGroups.system)}
+        {this.renderLabels(sortedLabels)}
+      </React.Fragment>
+    );
+  }
+
+  renderFolders(labels) {
+    const { t } = this.props;
+
+    const inboxLabel = {
+      ...labels.find(el => el.id === "INBOX"),
+      name: t("sidebar.inbox"),
+      icon: faInbox
+    };
+    const sentLabel = {
+      ...labels.find(el => el.id === "SENT"),
+      messagesUnread: 0,
+      name: t("sidebar.sent"),
+      icon: faEnvelopeSquare
+    };
+    const trashLabel = {
+      ...labels.find(el => el.id === "TRASH"),
+      messagesUnread: 0,
+      name: t("sidebar.trash"),
+      icon: faTrashAlt
+    };
+    const spamLabel = {
+      ...labels.find(el => el.id === "SPAM"),
+      name: t("sidebar.spam"),
+      icon: faExclamationTriangle
+    };
+
+   
+  }
+
+  renderLabels(labels) {
+    const { t } = this.props;
+
+    return (
+      <React.Fragment>       
+            <div className='calendar-control-section' style={{ overflow: 'auto' }}>
+                <CalendarComponent change={this.onchange} ></CalendarComponent>
+                <label id='date_label'>Selected Value:</label>
+            </div>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { t } = this.props;
+
+    const collapsed = this.props.sideBarCollapsed;
+
+    const composeProps = {
+      subject: "",
+      to: "",
+      content: ""
+    };
+
+    return (
+      //${ collapsed ? '' : styles['with-side-bar'] }
+      <nav
+        id="left-sidebar"
+        className={
+          collapsed
+            ? "d-flex flex-column text-truncate left-panel sidebar-close"
+            : "d-flex flex-column text-truncate left-panel sidebar-open"
+        }
+      >
+        <div className="compose-panel">
+          <div className="d-flex justify-content-center p-2 compose-btn">
+            <div className="compose-div">
+              <Link
+                className="btn font-weight-bold BtnLfcolor uppercase compose-btn"
+                to={{
+                  pathname: "/compose",
+                  search: "",
+                  state: { composeProps }
+                }}
+              >
+                <img
+                  className="ImgLf"
+                  border="0"
+                  alt="otulook"
+                  src="assets/img/plus.png"
+                ></img>
+                {t("calendar-sidebar.compose")}
+              </Link>
+              <Button
+                onClick={this.props.sideBarToggle}
+                className="btn-transparent margin-right-20 float-right margin-top-10"
+              >
+                <FontAwesomeIcon icon={faChevronLeft} size="1x" />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <PerfectScrollbar
+          component="ul"
+          className="d-flex flex-column border-0 m-0 sidebar"
+        >
+          {this.renderItems(this.props.labelsResult.labels)}
+        </PerfectScrollbar>
+      </nav>
+    );
+  }
+}
+
+//Sidebar.propTypes = {
+//    sideBarToggle: PropTypes.func.isRequired,
+//    collapsed: PropTypes.bool.isRequired
+//};
+
+//export default Sidebar;
+export default withTranslation()(Sidebar);
