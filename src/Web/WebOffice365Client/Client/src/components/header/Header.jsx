22f7d4d810538fb0ed3bf839c908714ea6764d7c
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-04-01 11:06:42 +0200

    AB#305 adding office 365 github menu control

--- src/Web/WebOffice365Client/Client/src/components/header/Header.jsx
+++ src/Web/WebOffice365Client/Client/src/components/header/Header.jsx
@@ -16,6 +16,7 @@ import { connect } from 'react-redux';
 import MenuUser from '../menu-user/menu-user';
 import i18n from 'i18next';
 import { resetDefaultAccount } from '../../api_graph/accounts';
+import MenuMinihub from '../menu-minihub/menu-minihub';
 
 export class Header extends PureComponent {
     constructor(props) {
@@ -128,53 +129,7 @@ export class Header extends PureComponent {
                         onSignout={this.props.onSignout}
                         picUrl={undefined}
                     />
-
-                    <div>
-                        <Nav className='ml-auto' navbar>
-                            <NavItem className='d-md-down-none'>
-                                <NavLink href='#'>
-                                    <i className='icon-location-pin'></i>
-                                </NavLink>
-                            </NavItem>
-                            <Dropdown
-                                nav
-                                isOpen={this.state.dropdownMenuOpen}
-                                toggle={this.toggleMenu}>
-                                <DropdownToggle nav>
-                                    <img
-                                        className='img-menu'
-                                        src='/assets/img/icon-products.png'
-                                        alt=''
-                                    />
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    {/* <DropdownItem>
-                    <i className="fa fa-shield"></i> Calendar
-                  </DropdownItem>
-                  <DropdownItem>
-                    <i className="fa fa-shield"></i> File Manager
-                  </DropdownItem>
-                  <DropdownItem>
-                    <i className="fa fa-shield"></i>
-                    <span onClick={this._handleOnClick}>
-                      {" "}
-                      Configure New Account
-                    </span>
-                  </DropdownItem>
-                  <DropdownItem divider /> */}
-                                    <DropdownItem>
-                                        <span
-                                            onClick={() =>
-                                                window.open('https://www.lex-on.es/', '_self')
-                                            }>
-                                            <i className='fa fa-lock'></i>
-                                            {i18n.t('header.returnlexon')}
-                                        </span>
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </Dropdown>
-                        </Nav>
-                    </div>
+                    <MenuMinihub />                 
                 </div>
             </header>
         );
