39b8aeb8176e116d28dcca71376f39fa3fa09540
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-04-13 10:00:17 +0200

    AB#606 fising bug

--- src/Web/WebGoogleClient/Client/src/components/main/Main.jsx
+++ src/Web/WebGoogleClient/Client/src/components/main/Main.jsx
@@ -129,7 +129,7 @@ export class Main extends Component {
           bbdd: this.props.lexon.bbdd,
           idCompany: this.props.lexon.idCompany,
           provider: this.props.lexon.provider,
-          account: googleUser.Qt.Au
+          account: googleUser.Qt.zu
         }
       })
     );
@@ -300,7 +300,7 @@ export class Main extends Component {
       idEmail = base64.decode(idEmail);
     }
 
-    const email = googleUser.Qt.Au;
+    const email = googleUser.Qt.zu;
 
     if (userId !== null && email !== null) {
       const user = await getUser(userId);
