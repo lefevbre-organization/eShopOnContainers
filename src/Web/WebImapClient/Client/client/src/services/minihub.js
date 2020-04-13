5ff65d0e34240aee654aa1dfa13995342c462509
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-04-13 10:58:36 +0200

    AB#611 fixing bugs for minihub

--- src/Web/WebImapClient/Client/client/src/services/minihub.js
+++ src/Web/WebImapClient/Client/client/src/services/minihub.js
@@ -3,7 +3,9 @@ export const getProducts = async userId => {
   
   //const url2 = `${window.API_GATEWAY}/api/v1/lex/Lexon/user?idUserNavision=${userId}`;
    
-    const url = `https://lexbox-test-apigwlex.lefebvre.es/api/v1/mysql/LexonMySql/user/apps?idNavisionUser=${userId}`;
+  //  const url = `https://lexbox-test-apigwlex.lefebvre.es/api/v1/mysql/LexonMySql/user/apps?idNavisionUser=${userId}`;
+
+    const url = `${window.API_GATEWAY}/api/v1/mysql/LexonMySql/user/apps?idNavisionUser=${userId}`;
 
   try {
     const res = await fetch(url, { method: 'GET' });
