daa6f7e39585c961564a1bbadd27ddfac5b01cd4
freyes-lef <f.reyes-ext@lefebvre.es>
2020-04-23 13:46:49 +0200

    separated userutils controllers

--- src/Services/UserUtils/UserUtils.API/Controllers/UserUtilsController.cs
+++ src/Services/UserUtils/UserUtils.API/Controllers/UserUtilsController.cs
@@ -26,168 +26,6 @@ namespace UserUtils.API.Controllers
             _settings = settings ?? throw new ArgumentNullException(nameof(settings));
             _service = service ?? throw new ArgumentNullException(nameof(service));
         }
-
-        /// <summary>
-        /// Permite obtener los token necesarios para operar con los microservicios de envio de correo
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-             string idClienteNavision = "E1621396"
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(idClienteNavision))
-                return BadRequest("id value invalid. Must be a valid user code in the enviroment or login and password");
-
-            var token = new TokenModelBase() { idClienteNavision = idClienteNavision };
-
-            var result = await _service.GetTokenAsync(token, addTerminatorToToken);
-
-            return result.errors?.Count > 0 ? (IActionResult)BadRequest(result) : Ok(result);
-
-        }
-
-        /// <summary>
-        /// Permite obtener los token necesarios mediante login y password y eligiendo la aplicación adecuada
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-            [FromBody] TokenRequestLogin tokenRequest
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(tokenRequest.login) && string.IsNullOrEmpty(tokenRequest.password))
-                return BadRequest("Must be a valid login and password");
-
-            Result<TokenData> result = await _service.GetUserFromLoginAsync(
-                tokenRequest.idApp, tokenRequest.login, tokenRequest.password, addTerminatorToToken);
-            
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-
-        }
-
-        /// <summary>
-        /// Permite obtener los token necesarios mediante un idUsarioNavision
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-            [FromBody] TokenRequest tokenRequest
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(tokenRequest.idClienteNavision))
-                return BadRequest("Must be a valid idClient");
-
-            Result<TokenData> result = await _service.GetLexonUserSimpleAsync(
-                tokenRequest.idClienteNavision, addTerminatorToToken);
-
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-
-        }
-
-        /// <summary>
-        /// Permite obtener los token necesarios mediante un idUsarioNavision
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-            [FromBody] TokenRequestDataBase tokenRequest
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(tokenRequest.idClienteNavision) || string.IsNullOrEmpty(tokenRequest.bbdd))
-                return BadRequest("Must be a valid idClient and bbdd");
-
-            Result<TokenData> result = await _service.GetLexonUserDbAsync(
-                tokenRequest, addTerminatorToToken);
-
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-
-        }
-
-        /// <summary>
-        /// Permite obtener los token necesarios para crear un nuevo mail
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-            [FromBody] TokenRequestNewMail tokenRequest
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(tokenRequest.idClienteNavision))
-                return BadRequest("Must be a valid idClient");
-
-            Result<TokenData> result = await _service.GetLexonNewMailAsync(
-                tokenRequest, addTerminatorToToken);
-
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-
-        }
-
-        /// <summary>
-        /// Permite obtener los token necesarios para crear un nuevo mail
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenAsync(
-            [FromBody] TokenRequestOpenMail tokenRequest
-            , bool addTerminatorToToken = true
-            )
-        {
-            if (string.IsNullOrEmpty(tokenRequest.idClienteNavision) 
-                || string.IsNullOrEmpty(tokenRequest.idMail))
-                return BadRequest("Must be a valid idClient and valid idMail");
-
-            Result<TokenData> result = await _service.GetLexonOpenMailAsync(
-                tokenRequest, addTerminatorToToken);
-
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-
-        }
-
-
-        /// <summary>
-        /// Permite obtener los token necesarios para operar con los microservicios de envio de correo
-        /// </summary>
-        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
-        /// <returns></returns>
-        [HttpPut("token/validation")]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> TokenValidationAsync(
-             [FromBody] string token
-            )
-        {
-            if (string.IsNullOrEmpty(token))
-                return BadRequest("Must be a valid token to validate");
-
-            var tokenRequest = new TokenData() { token = token, valid = false };
-            var result = await _service.VadidateTokenAsync(tokenRequest);
-            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
-        }
-
         /// <summary>
         /// Permite obtener los token necesarios para operar con los microservicios de envio de correo
         /// </summary>
@@ -213,6 +51,26 @@ namespace UserUtils.API.Controllers
             var result = await _service.GetTokenAsync(tokenRequest, addTerminatorToToken);
 
             return result.errors?.Count > 0 ? (IActionResult)BadRequest(result) : Ok(result);
+        }
+
+        /// <summary>
+        /// Permite obtener los token necesarios para operar con los microservicios de envio de correo
+        /// </summary>
+        /// <param name="addTerminatorToToken">opcional, agrega un slash para ayudar a terminar la uri</param>
+        /// <returns></returns>
+        [HttpPut("token/validation")]
+        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Result<TokenData>), (int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> TokenValidationAsync(
+             [FromBody] string token
+            )
+        {
+            if (string.IsNullOrEmpty(token))
+                return BadRequest("Must be a valid token to validate");
+
+            var tokenRequest = new TokenData() { token = token, valid = false };
+            var result = await _service.VadidateTokenAsync(tokenRequest);
+            return result.data.valid ? Ok(result) : (IActionResult)BadRequest(result);
         }
 
 
