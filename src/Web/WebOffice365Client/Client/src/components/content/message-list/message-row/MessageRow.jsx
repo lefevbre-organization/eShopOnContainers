930ebabe0e3e5cddf17a5ced364f4c7976731be7
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-05-07 11:38:06 +0200

    AB#696

--- src/Web/WebOffice365Client/Client/src/components/content/message-list/message-row/MessageRow.jsx
+++ src/Web/WebOffice365Client/Client/src/components/content/message-list/message-row/MessageRow.jsx
@@ -1,11 +1,11 @@
-import React, { PureComponent } from "react";
-import { withRouter } from "react-router-dom";
-import moment from "moment";
-import MesssageCheckbox from "./MessageCheckbox";
+import React, { PureComponent } from 'react';
+import { withRouter } from 'react-router-dom';
+import moment from 'moment';
+import MesssageCheckbox from './MessageCheckbox';
 
-import NameSubjectFields from "./NameSubjectFields";
-import AttachmentDateFields from "./AttachmentDateFields";
-import { getNameEmail } from "../../../../utils";
+import NameSubjectFields from './NameSubjectFields';
+import AttachmentDateFields from './AttachmentDateFields';
+import { getNameEmail } from '../../../../utils';
 
 export class MessageItem extends PureComponent {
   constructor(props) {
@@ -29,38 +29,42 @@ export class MessageItem extends PureComponent {
     return nameEmail.name;
   }
 
-    getFormattedDate(date, fallbackDateObj) {
-
+  getFormattedDate(date, fallbackDateObj) {
     let messageDate = moment(date);
     var locale = window.navigator.userLanguage || window.navigator.language;
-        if(locale === "es-ES")
-        {
-            moment.lang('es', {
-                months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
-                monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
-                weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
-                weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
-                weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
-            });
-        }  
+    if (locale === 'es-ES') {
+      moment.lang('es', {
+        months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split(
+          '_'
+        ),
+        monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split(
+          '_'
+        ),
+        weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split(
+          '_'
+        ),
+        weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+        weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_'),
+      });
+    }
 
     if (!messageDate.isValid()) {
       messageDate = moment(fallbackDateObj.parserFn(fallbackDateObj.date));
     }
     const nowDate = moment(new Date());
     const isMessageFromToday =
-      messageDate.format("YYYYMMDD") === nowDate.format("YYYYMMDD");
+      messageDate.format('YYYYMMDD') === nowDate.format('YYYYMMDD');
     let formattedDate;
     if (isMessageFromToday) {
-      formattedDate = messageDate.format("h:mm A");
+      formattedDate = messageDate.format('h:mm A');
     } else {
       if (messageDate.year() !== nowDate.year()) {
-        formattedDate = messageDate.format("YYYY/MM/DD");
+        formattedDate = messageDate.format('YYYY/MM/DD');
       } else {
-        formattedDate = messageDate.format("MMM D");
+        formattedDate = messageDate.format('MMM D');
       }
-      }
-    
+    }
+
     return formattedDate;
   }
 
@@ -68,12 +72,12 @@ export class MessageItem extends PureComponent {
     const date = this.props.data.receivedDateTime;
     let formattedDate = this.getFormattedDate(date, {
       date: this.props.data.internalDate,
-      parserFn: parseInt
+      parserFn: parseInt,
     });
-    const unread = this.props.data.isRead === false ? " font-weight-bold" : "";
-    const selected = "";
+    const unread = this.props.data.isRead === false ? ' font-weight-bold' : '';
+    const selected = '';
     const subject = this.props.data.subject;
-    let fromName = "";
+    let fromName = '';
 
     //For unusual cases taht the data.from.emailAddress not exist -  to-do Alberto
     //(ask for undefined object)
@@ -82,12 +86,11 @@ export class MessageItem extends PureComponent {
       //   this.props.data.from.emailAddress.name.toUpperCase() +
       //   " " +
       //   this.props.data.from.emailAddress.address;
-      if(this.props.isSent === false) {
+      if (this.props.isSent === false) {
         fromName = this.props.data.from.emailAddress.name;
       } else {
         fromName = this.props.data.toRecipients[0].emailAddress.name;
       }
-
     } catch {
       // const fromHeader = "";
     }
@@ -100,9 +103,13 @@ export class MessageItem extends PureComponent {
         />
         <div
           onClick={this.getMessage}
-          className={`table-row px-2 py-3${unread}`}
-        >
+          className={`table-row px-2 py-3${unread}`}>
           <NameSubjectFields fromName={fromName} subject={subject} />
+          {this.props.data.importance === 'high' && (
+            <div>
+              <i className='lf-icon-bookmarks-active'></i>
+            </div>
+          )}
           <AttachmentDateFields
             formattedDate={formattedDate}
             hasAttachment={
