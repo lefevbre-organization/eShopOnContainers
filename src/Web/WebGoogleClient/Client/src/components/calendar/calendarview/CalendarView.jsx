ece671361a61f92a2571d44afc2d46632e88bb2b
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-05-14 16:10:49 +0200

    fixing error of listview syncfusion

--- src/Web/WebGoogleClient/Client/src/components/calendar/calendarview/CalendarView.jsx
+++ src/Web/WebGoogleClient/Client/src/components/calendar/calendarview/CalendarView.jsx
@@ -10,32 +10,32 @@ import './calendarview.scss';
 export class CalendarView extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {           
-            calendarid: this.props.calendarToEdit,           
-            hideAddCalendarButton: false,           
-        };  
+        this.state = {
+            calendarid: this.props.calendarToEdit,
+            hideAddCalendarButton: false,
+        };
         this.onAddClick = this.onAddClick.bind(this);
         this.onModifyClick = this.onModifyClick.bind(this);
         this.onAddPermission = this.onAddPermission.bind(this);
-        this.getlistACL = this.getlistACL.bind(this);     
-       
+        this.getlistACL = this.getlistACL.bind(this);
+
 
         this.toasts = [
-            { content: 'Processing'},
+            { content: 'Processing' },
             { content: 'Process complete', cssClass: 'e-toast-success', icon: 'e-success toast-icons' },
             { content: 'Error', cssClass: 'e-toast-danger', icon: 'e-error toast-icons' }
         ]
-        this.position = { X: 'Center', Y: 'Bottom' };  
+        this.position = { X: 'Center', Y: 'Bottom' };
 
 
         this.roleData = [
             { "Id": "freeBusyReader", "Role": "freeBusyReader" },
             { "Id": "reade", "Role": "reader" },
             { "Id": "writer", "Role": "writer" },
-            { "Id": "owner", "Role": "owner" }           
+            { "Id": "owner", "Role": "owner" }
         ];
-        this.temp = 'roleData';       
-        this.fields = { text: 'Role', value: 'Id' };       
+        this.temp = 'roleData';
+        this.fields = { text: 'Role', value: 'Id' };
         this.value = "reader";
 
         this.listviewInstance = null;
@@ -50,8 +50,8 @@ export class CalendarView extends React.Component {
         show: { effect: 'SlideBottomIn' }
     };
 
-    onAddClick(args) { 
-        this.setState({ buttonDisabled: true } )
+    onAddClick(args) {
+        this.setState({ buttonDisabled: true })
 
         let calendar = {
             "summary": this.nameObj.value,
@@ -74,10 +74,10 @@ export class CalendarView extends React.Component {
                 console.log('error ->', error);
                 this.toastObj.showProgressBar = false
                 this.toastObj.hide('All');
-                this.toastObj.show(this.toasts[2]); 
+                this.toastObj.show(this.toasts[2]);
                 this.addBtn.properties.disabled = false
                 this.setState({ buttonDisabled: false })
-            });       
+            });
     }
 
     onModifyClick(args) {
@@ -106,9 +106,9 @@ export class CalendarView extends React.Component {
                 this.toastObj.show(this.toasts[2]);
                 this.addBtn.properties.disabled = false
                 this.setState({ buttonDisabled: false })
-            });       
-       
-    }  
+            });
+
+    }
 
     onAddPermission(args) {
         let id = "user:" + this.mailaccountObj.value;
@@ -117,7 +117,7 @@ export class CalendarView extends React.Component {
             "scope":
             {
                 "type": "user",
-                    "value": this.mailaccountObj.value
+                "value": this.mailaccountObj.value
             },
             "kind": "calendar#aclRule",
             "id": id
@@ -133,13 +133,13 @@ export class CalendarView extends React.Component {
                 this.toastObj.show(this.toasts[1]);
 
                 let dataPermission = {
-                    text: this.mailaccountObj.value ,
+                    text: this.mailaccountObj.value,
                     role: this.permissionObj.value,
                     id: id,
                     icon: "delete-icon"
                 };
                 this.listviewInstance.addItem([dataPermission]);
-               
+
             })
             .catch(error => {
                 console.log('error ->', error);
@@ -148,41 +148,41 @@ export class CalendarView extends React.Component {
                 this.toastObj.show(this.toasts[2]);
                 this.addBtn.properties.disabled = false
                 this.setState({ buttonDisabled: false })
-            });   
+            });
     }
 
-    onDataBinding(items) {      
+    onDataBinding(items) {
         if (items.length > 0) {
             for (let i = 0; i < items.length; i++) {
-                let acl = items[i];             
-                
+                let acl = items[i];
+
                 this.dataACLSource.push({
                     text: acl.scope.value,
                     id: acl.id,
                     role: acl.role,
-                    icon: "delete-icon"             
+                    icon: "delete-icon"
                 });
             }
         }
-       // this.listviewInstance = this.dataACLSource;
+        // this.listviewInstance = this.dataACLSource;
         this.listviewInstance.refresh();
-       // this.listviewInstance.refreshChild();
+        // this.listviewInstance.refreshChild();
     }
 
-    getlistACL() {       
+    getlistACL() {
         listACL(this.state.calendarid)
             .then(result => {
-               this.onDataBinding(result.items)
+                this.onDataBinding(result.items)
             })
             .catch(error => {
-                console.log('error ->', error);                
+                console.log('error ->', error);
             });
     }
 
     listTemplate(data) {
         let owner = false
         if (data.role == "owner") {
-            owner=true
+            owner = true
         }
 
         return (<div className="text-content">
@@ -195,7 +195,7 @@ export class CalendarView extends React.Component {
 
         </div>);
     }
-   
+
     deleteItem(args) {
         args.stopPropagation();
         let liItem = args.target.parentElement.parentElement;
@@ -206,17 +206,17 @@ export class CalendarView extends React.Component {
             .then(result => {
                 this.toastObj.hide('All');
                 this.toastObj.showProgressBar = false
-                this.toastObj.show(this.toasts[1]); 
-               this.listviewInstance.removeItem(liItem);
+                this.toastObj.show(this.toasts[1]);
+                this.listviewInstance.removeItem(liItem);
             })
             .catch(error => {
-                console.log('error ->', error);  
+                console.log('error ->', error);
                 this.toastObj.showProgressBar = false
                 this.toastObj.hide('All');
                 this.toastObj.show(this.toasts[2]);
-                           
-            });   
-       
+
+            });
+
     }
 
     componentDidMount() {
@@ -224,18 +224,18 @@ export class CalendarView extends React.Component {
         if (this.props.calendarToEdit != "") {
             this.setState({ hideAddCalendarButton: true });
             getCalendar(this.props.calendarToEdit)
-               .then(result => { 
+                .then(result => {
                     this.nameObj.value = result.summary;
                     if (result.description !== undefined) {
                         this.descriptionObj.value = result.description;
-                    }                   
-               })
-               .catch(error => {
-                  console.log('error ->', error);
-               });
+                    }
+                })
+                .catch(error => {
+                    console.log('error ->', error);
+                });
 
             this.getlistACL();
-        }         
+        }
     }
 
     onChange() {
@@ -244,7 +244,7 @@ export class CalendarView extends React.Component {
         //value.innerHTML = this.permissionObj.value === null ? 'null' : this.permissionObj.value.toString();
         //text.innerHTML = this.permissionObj.text === null ? 'null' : this.permissionObj.text;
     }
-    
+
     rendereComplete() {
         this.onChange();
     }
@@ -253,32 +253,32 @@ export class CalendarView extends React.Component {
 
         var ObjClick;
         var ObjText;
-        if (this.props.calendarToEdit !='') {
+        if (this.props.calendarToEdit != '') {
             ObjClick = this.onModifyClick
             ObjText = 'Modify'
         }
         else {
             ObjClick = this.onAddClick
             ObjText = 'Add Calendar'
-        } 
-      
+        }
+
 
         return (
             <div className="row custom-margin custom-padding-5 material2">
-                <div className="col-xs-12 col-sm-12 col-lg-12 col-md-12">                     
-                    <div id="formComponents">                       
+                <div className="col-xs-12 col-sm-12 col-lg-12 col-md-12">
+                    <div id="formComponents">
                         <div className='validation_wrapper'>
-                            <div className="form-horizontal"> 
-                                    <div className="form-group">
-                                        <div className="e-float-input">
-                                           <TextBoxComponent
-                                               id='name'
-                                               placeholder="Name"
-                                               floatLabelType="Always"                                                   
-                                               ref={(scope) => { this.nameObj = scope }}
-                                           />
-                                        </div>
-                                    <div id="userError" /> 
+                            <div className="form-horizontal">
+                                <div className="form-group">
+                                    <div className="e-float-input">
+                                        <TextBoxComponent
+                                            id='name'
+                                            placeholder="Name"
+                                            floatLabelType="Always"
+                                            ref={(scope) => { this.nameObj = scope }}
+                                        />
+                                    </div>
+                                    <div id="userError" />
                                 </div>
                                 <div className="form-group">
                                     <div className="e-float-input">
@@ -292,7 +292,7 @@ export class CalendarView extends React.Component {
                                         />
                                     </div>
                                     <div id="noError" />
-                                </div> 
+                                </div>
                                 <div>
 
                                     {/* <div className="row">
@@ -309,43 +309,43 @@ export class CalendarView extends React.Component {
                                             </div>
                                     </div>*/}
 
-                                </div> 
+                                </div>
                             </div>
-                            
-                           
+
+
 
                             {this.state.hideAddCalendarButton ? (
-                               
+
                                 <div >
                                     <h4 className="e-dlg-header">Share with people</h4>
-                                        <ListViewComponent
-                                            id="sample-list"
-                                            dataSource={this.dataACLSource}
-                                            fields={this.fieldsList}
-                                            template={this.listTemplate.bind(this)} ref={listview => {
-                                                this.listviewInstance = listview;
+                                    <ListViewComponent
+                                        id="sample-list"
+                                        dataSource={this.dataACLSource}
+                                        fields={this.fieldsList}
+                                        template={this.listTemplate.bind(this)} ref={listview => {
+                                            this.listviewInstance = listview;
                                         }} />
 
                                     {/*<ButtonComponent id="btn" onClick={this.addItem.bind(this)}>
                                         Add Item
                                     </ButtonComponent>*/}
 
-                                   
-                                    <div className="row">                                        
-                                        <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6"> 
+
+                                    <div className="row">
+                                        <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6">
                                             <div className="form-group">
-                                                <div className="e-float-input">                                                    
-                                                     <TextBoxComponent
-                                                         id='mail'
-                                                         placeholder="Mail address"
-                                                         floatLabelType="Always"
-                                                         ref={(scope) => { this.mailaccountObj = scope }}
-                                                     />                                                   
+                                                <div className="e-float-input">
+                                                    <TextBoxComponent
+                                                        id='mail'
+                                                        placeholder="Mail address"
+                                                        floatLabelType="Always"
+                                                        ref={(scope) => { this.mailaccountObj = scope }}
+                                                    />
                                                 </div>
                                                 <div id="noError" />
-                                            </div> 
+                                            </div>
                                         </div>
-                                        <div className="col-xs-4 col-sm-4 col-lg-4 col-md-4"> 
+                                        <div className="col-xs-4 col-sm-4 col-lg-4 col-md-4">
                                             <div className="form-group">
                                                 <div className="e-float-input">
                                                     <ComboBoxComponent
@@ -360,9 +360,9 @@ export class CalendarView extends React.Component {
                                                         popupHeight="220px" />
                                                 </div>
                                                 <div id="noError" />
-                                            </div> 
-                                        </div> 
-                                        <div className="col-xs-2 col-sm-2 col-lg-2 col-md-2"> 
+                                            </div>
+                                        </div>
+                                        <div className="col-xs-2 col-sm-2 col-lg-2 col-md-2">
                                             <div className="e-float-input buttonadd">
                                                 <ButtonComponent
                                                     cssClass='e-flat e-primary'
@@ -370,13 +370,13 @@ export class CalendarView extends React.Component {
                                                     onClick={this.onAddPermission}
                                                 >Add</ButtonComponent>
                                             </div>
-                                        </div> 
+                                        </div>
                                     </div>
-                                    
+
 
                                 </div >
                             ) : (
-                                   ''
+                                    ''
                                 )}
 
                             <div className="row">
@@ -405,7 +405,7 @@ export class CalendarView extends React.Component {
                         </div>
                     </div>
                 </div>
-            </div> 
+            </div>
         )
     }
 }
