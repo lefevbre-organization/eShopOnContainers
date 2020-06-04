3b275d49e5ed7a61cdffcccdbafc195a2e566968
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-06-04 14:56:27 -0600

    Integrate new auth api on gmail addon

--- src/Addon/Lexon/Gmail/src/config.js
+++ src/Addon/Lexon/Gmail/src/config.js
@@ -1,7 +1,7 @@
 var urlFrontend = "https://lexbox-test-webaddonlexon.lefebvre.es/";
 // var urlFrontend = "https://localhost:3000/";
 var apiEndpoint = "https://lexbox-test-apigwlex.lefebvre.es/api/v1/lex/Lexon/";
-var apiAccount = "https://lexbox-test-apigwacc.lefebvre.es/api/v2/accounts/usermail/E1621396/raw";
+var apiAccount = "https://lexbox-test-apigwacc.lefebvre.es/api/v2/accounts/usermail/";
 var clientId = "871221881170-tt724p6m481ad5kbpa3f0udv4j1q3558.apps.googleusercontent.com";
 var clientSecret = "H-7TJXACoa5JdCCXl_dILSEz";
 var urlAuth = "https://lexbox-test-apigwlex.lefebvre.es/api/v1/mysql/LexonMySql/";
