b95d49c5e60c08caf08e6c09152ac3f51b81baf0
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-06-05 17:15:23 +0200

    adding new paramenter in addon

--- src/Web/WebAddonLexon/Client/public/config.js
+++ src/Web/WebAddonLexon/Client/public/config.js
@@ -1,5 +1,5 @@
 window.REACT_APP_ENVIRONMENT = "LEXON-CLIENT LOCAL";
-window.GOOGLE_SCRIPT = "https://script.google.com/macros/d/1rzrejrBXzshc4OWW_8xqAwOBJ4avJocCqN9HUBtDSo7U1KZjpqKnXyXo/usercallback"
+window.GOOGLE_SCRIPT = "https://script.google.com/macros/d/17OVCKEejIyIITkMSVlF3KJelJFI1ouLFeoS2Wj4sO7U4hlS1URiK_Ntk/usercallback"
 window.URL_MF_LEXON = "http://localhost:3004/static/js/main.js"
 window.URL_MF_LEXON_BASE = "http://localhost:3004/"
 window.API_GATEWAY='https://lexbox-test-apigwlex.lefebvre.es'
