af2ef7dbb0665d16351cb6634d202015cca21b0e
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-06-10 13:15:22 +0200

    Fix portal style

--- src/Web/WebPortalClient/Client/src/components/header/Header.jsx
+++ src/Web/WebPortalClient/Client/src/components/header/Header.jsx
@@ -10,31 +10,63 @@ export class Header extends PureComponent {
 
     return (
       <header className='d-flex p-3 align-content-center align-items-center header '>
-        <div className='header-left'>
-          <img src={logoHeader} alt='logo' />
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xs-12 col-md-6 header-logo'>
+              <a
+                rel='noopener noreferrer'
+                href='https://lefebvre.es'
+                target='_blank'>
+                <img src={logoHeader} alt='LEFEVBRE' />
+              </a>
+            </div>
+            <div className='col-xs-12 col-md-6 header-icons'>
+              {showUser && (
+                <div className='header-user'>
+                  <div>
+                    <span className='lf-icon-user'></span>
+                  </div>
+                  <div>
+                    <span className='user-name'>{userName}</span>
+                  </div>
+                </div>
+              )}
+              {!showUser && (
+                <a
+                  href='https://www.efl.es/'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <span className='lf-icon-shop'>
+                    <span className='shop-text'>{i18n.t('header.shop')}</span>
+                  </span>
+                </a>
+              )}
+            </div>
+            {/* <img src={logoHeader} alt='logo' />
+            {showUser && (
+              <div className='header-user'>
+                <div>
+                  <span className='lf-icon-user'></span>
+                </div>
+                <div>
+                  <span className='user-name'>{userName}</span>
+                </div>
+              </div>
+            )}
+            {!showUser && (
+              <div className='header-user'>
+                <a
+                  href='https://www.efl.es/'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <span className='lf-icon-shop'>
+                    <span className='shop-text'>{i18n.t('header.shop')}</span>
+                  </span>
+                </a>
+              </div>
+            )} */}
+          </div>
         </div>
-        {showUser && (
-          <div className='header-user'>
-            <div>
-              <span className='lf-icon-user'></span>
-            </div>
-            <div>
-              <span className='user-name'>{userName}</span>
-            </div>
-          </div>
-        )}
-        {!showUser && (
-          <div className='header-user'>
-            <a
-              href='https://www.efl.es/'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <span className='lf-icon-shop'>
-                <span className='shop-text'>{i18n.t('header.shop')}</span>
-              </span>
-            </a>
-          </div>
-        )}
       </header>
     );
   }
