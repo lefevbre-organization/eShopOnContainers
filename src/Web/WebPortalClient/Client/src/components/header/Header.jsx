2923c1d43e8c34fa74b331628d6e3eb6ce06c90e
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-06-10 17:12:28 +0200

    Fix portal styles

--- src/Web/WebPortalClient/Client/src/components/header/Header.jsx
+++ src/Web/WebPortalClient/Client/src/components/header/Header.jsx
@@ -9,40 +9,54 @@ export class Header extends PureComponent {
     const { userName, showUser = true } = this.props;
 
     return (
-      <header className='d-flex p-3 align-content-center align-items-center header '>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xs-12 col-md-6 header-logo'>
-              <a
-                rel='noopener noreferrer'
-                href='https://lefebvre.es'
-                target='_blank'>
-                <img src={logoHeader} alt='LEFEVBRE' />
-              </a>
-            </div>
-            <div className='col-xs-12 col-md-6 header-icons'>
-              {showUser && (
-                <div className='header-user'>
-                  <div>
-                    <span className='lf-icon-user'></span>
-                  </div>
-                  <div>
-                    <span className='user-name'>{userName}</span>
-                  </div>
-                </div>
-              )}
-              {!showUser && (
-                <a
-                  href='https://www.efl.es/'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  <span className='lf-icon-shop'>
-                    <span className='shop-text'>{i18n.t('header.shop')}</span>
-                  </span>
-                </a>
-              )}
-            </div>
-            {/* <img src={logoHeader} alt='logo' />
+      <header className='login-header'>
+        <div className='front-login__header-login row'>
+          <a class='front-login__go-home col-md-5'>
+            <img
+              src='https://assets.lefebvre.es/media/logos/web/comunes/lefebvre-bl-120x24.png'
+              className='front-login__logo-lefebvre'
+            />
+          </a>
+          <a
+            class='front-login__go-shop'
+            href='https://www.efl.es/'
+            target='_blank'>
+            <i class='lf-icon-shop front-login__go-shop-icon'></i>
+            <span data-translate='front-login.shop' class='ng-scope'>
+              TIENDA
+            </span>
+          </a>
+        </div>
+      </header>
+
+      // <header className='d-flex p-3 align-content-center align-items-center header'>
+      //   <div className='col-xs-12 col-md-5 header-logo'>
+      //     <a
+      //       rel='noopener noreferrer'
+      //       href='https://lefebvre.es'
+      //       target='_blank'>
+      //       <img src={logoHeader} alt='LEFEVBRE' />
+      //     </a>
+      //   </div>
+      //   <div className='col-xs-12 col-md-5 header-logo'>
+      //     {showUser && (
+      //       <div>
+      //         <span className='lf-icon-user'></span>
+      //         <span className='user-name'>{userName}</span>
+      //       </div>
+      //     )}
+      //     {!showUser && (
+      //       <a
+      //         href='https://www.efl.es/'
+      //         target='_blank'
+      //         rel='noopener noreferrer'>
+      //         <span className='lf-icon-shop'>
+      //           <span className='shop-text'>{i18n.t('header.shop')}</span>
+      //         </span>
+      //       </a>
+      //     )}
+      //   </div>
+      /* <img src={logoHeader} alt='logo' />
             {showUser && (
               <div className='header-user'>
                 <div>
@@ -64,10 +78,7 @@ export class Header extends PureComponent {
                   </span>
                 </a>
               </div>
-            )} */}
-          </div>
-        </div>
-      </header>
+            )} */
     );
   }
 }
