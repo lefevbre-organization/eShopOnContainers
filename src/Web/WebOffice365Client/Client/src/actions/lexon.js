0871164c7acc100b9ed7066e35c104e518d6dc24
Juan del Valle <j.delvalle-ext@lefebvre.es>
2020-06-10 08:39:17 +0200

    AB#811

--- src/Web/WebOffice365Client/Client/src/actions/lexon.js
+++ src/Web/WebOffice365Client/Client/src/actions/lexon.js
@@ -1,61 +1,68 @@
 export const ActionTypes = {
-    LEXON_USER: 'LEXON_USER',
-    LEXON_CASEFILE: 'LEXON_CASEFILE',
-    LEXON_ACCOUNT: 'LEXON_ACCOUNT',
-    LEXON_MAILCONTACTS: 'LEXON_MAILCONTACTS',
-    LEXON_ACCOUNT_GUID: 'LEXON_ACCOUNT_GUID',
-    LEXON_ACCOUNT_SIGN: 'LEXON_ACCOUNT_SIGN',
-    LEXON_IDMAIL: 'LEXON_IDMAIL',
-    LEXON_TOKEN: 'LEXON_TOKEN'
+  LEXON_USER: 'LEXON_USER',
+  LEXON_CASEFILE: 'LEXON_CASEFILE',
+  LEXON_ACCOUNT: 'LEXON_ACCOUNT',
+  LEXON_BBDD: 'LEXON_BBDD',
+  LEXON_MAILCONTACTS: 'LEXON_MAILCONTACTS',
+  LEXON_ACCOUNT_GUID: 'LEXON_ACCOUNT_GUID',
+  LEXON_ACCOUNT_SIGN: 'LEXON_ACCOUNT_SIGN',
+  LEXON_IDMAIL: 'LEXON_IDMAIL',
+  LEXON_TOKEN: 'LEXON_TOKEN',
 };
 
-export const setUser = user => ({
-    type: ActionTypes.LEXON_USER,
-    payload: user
+export const setUser = (user) => ({
+  type: ActionTypes.LEXON_USER,
+  payload: user,
 });
 
-export const setCaseFile = casefile => ({
-    type: ActionTypes.LEXON_CASEFILE,
-    payload: casefile
+export const setCaseFile = (casefile) => ({
+  type: ActionTypes.LEXON_CASEFILE,
+  payload: casefile,
 });
 
-export const setAccount = account => ({
-    type: ActionTypes.LEXON_ACCOUNT,
-    payload: account
+export const setAccount = (account) => ({
+  type: ActionTypes.LEXON_ACCOUNT,
+  payload: account,
 });
 
-export const setMailContacts = mailContacts => ({
-    type: ActionTypes.LEXON_MAILCONTACTS,
-    payload: mailContacts
+export const setBBDD = (account) => ({
+  type: ActionTypes.LEXON_BBDD,
+  payload: account,
 });
 
-export const setGUID = guid => ({
-    type: ActionTypes.LEXON_ACCOUNT_GUID,
-    payload: guid
+export const setMailContacts = (mailContacts) => ({
+  type: ActionTypes.LEXON_MAILCONTACTS,
+  payload: mailContacts,
 });
 
-export const setSign = sign => ({
-    type: ActionTypes.LEXON_ACCOUNT_SIGN,
-    payload: sign
+export const setGUID = (guid) => ({
+  type: ActionTypes.LEXON_ACCOUNT_GUID,
+  payload: guid,
 });
 
-export const setIdMail = idMail => ({
-    type: ActionTypes.LEXON_IDMAIL,
-    payload: idMail
+export const setSign = (sign) => ({
+  type: ActionTypes.LEXON_ACCOUNT_SIGN,
+  payload: sign,
 });
 
-export const setToken = token => ({
-    type: ActionTypes.LEXON_TOKEN,
-    payload: token
+export const setIdMail = (idMail) => ({
+  type: ActionTypes.LEXON_IDMAIL,
+  payload: idMail,
+});
+
+export const setToken = (token) => ({
+  type: ActionTypes.LEXON_TOKEN,
+  payload: token,
 });
 
 export default {
-    setUser,
-    setCaseFile,
-    setAccount,
-    setMailContacts,
-    setGUID,
-    setSign,
-    setIdMail,
-    setToken
+  setUser,
+  setCaseFile,
+  setAccount,
+  setMailContacts,
+  setGUID,
+  setSign,
+  setIdMail,
+  setToken,
+  setBBDD,
 };
