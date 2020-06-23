a4c2634b4837808d272f85ba8617d0d884f6de39
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-06-23 10:52:42 -0600

    new change on manifest.xml

--- src/Addon/Lexon/Office365/WebOffice365Addon/Client/public/config.js
+++ src/Addon/Lexon/Office365/WebOffice365Addon/Client/public/config.js
@@ -1,2 +1,3 @@
 window.API_GATEWAY='https://lexbox-test-apigwlex.lefebvre.es/api/v1/lex/Lexon';
 window.URL_GET_ACCOUNTS = "https://lexbox-test-apigwlex.lefebvre.es/api/v2/accounts/usermail";
+window.URL_MAIN = "https://lexbox-test-weboffice365addonlexon.lefebvre.es/";
