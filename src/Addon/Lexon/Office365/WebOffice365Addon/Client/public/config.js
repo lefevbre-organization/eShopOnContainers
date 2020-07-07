9d1fbfcacf242f15c2e5c926156aa67e403c91ab
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-07-07 11:45:00 -0600

    proyect renamed WebOffice365AddonLexon and dockerfiles configuration

--- src/Addon/Lexon/Office365/WebOffice365Addon/Client/public/config.js
+++
@@ -1,4 +0,0 @@
-window.URL_ADDON_LEXON_BASE = "https://lexbox-test-weboffice365addonlexon.lefebvre.es"
-window.URL_ADDON_LEXON = 'https://lexbox-test-webaddonlexon.lefebvre.es'
-window.API_GATEWAY='https://lexbox-test-apigwlex.lefebvre.es/api/v1';
-window.URL_GET_ACCOUNTS = "https://lexbox-test-apigwlex.lefebvre.es/api/v2/accounts/usermail";
