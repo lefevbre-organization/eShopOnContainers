6ae68f3b655878d4d218358f4611c6c781c9f6fd
freyes-lef <f.reyes-ext@lefebvre.es>
2020-07-13 13:02:33 +0200

    fix error online in database.api

--- src/BuildingBlocks/Models/Lef.Models/Online/OnlineEntrada.cs
+++ src/BuildingBlocks/Models/Lef.Models/Online/OnlineEntrada.cs
@@ -1,10 +1,10 @@
 ﻿namespace Microsoft.eShopOnContainers.BuildingBlocks.Lefebvre.Models
 {
-    //public class OnlineEntrada
-    //{
-    //    public string ENTRADA_ENCRIPTADA { get; set; }
-    //    public long ID_ENTRADA { get; set; }
-    //    public string N_ENTRADA { get; set; }
-    //}
+    public class OnlineEntrada
+    {
+        public string ENTRADA_ENCRIPTADA { get; set; }
+        public long ID_ENTRADA { get; set; }
+        public string N_ENTRADA { get; set; }
+    }
 
 }
