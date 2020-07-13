6a3baa1e82c8ab3862e356c9a2bec2ef16c738ae
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-07-13 08:49:55 -0600

    adding centinela login on webaddonlauncher

--- src/Web/WebAddonLauncher/Client/src/services/services-lexon.js
+++ src/Web/WebAddonLauncher/Client/src/services/services-lexon.js
@@ -1,7 +1,7 @@
 export const getUser = async (
     data
   ) => {
-    const url = `${window.URL_GET_USER}`
+    const url = `${window.URL_GET_USER}/user/login`
     + '?login='+ data.login + '&pass=' + 
     data.password;
   
@@ -18,4 +18,25 @@ export const getUser = async (
     } catch (err) {
       throw err;
     }
-  };
+};
+
+export const getCentinelaUser = async (data) => {
+  const url = `${window.URL_GET_USER}/token`;
+
+  let formData = new FormData();
+
+  formData.append('login', data.login);
+  formData.append('password', data.password);
+  formData.append('idApp', 2);
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    });
+    const result = await response.json();
+    return { result };
+  } catch (err) {
+    throw err;
+  }
+}
