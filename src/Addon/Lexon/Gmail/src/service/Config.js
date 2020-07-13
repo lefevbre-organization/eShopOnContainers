1939a5592a56fe9513d4a9434c8f1f0252e5c2fc
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-07-13 13:52:50 -0600

    remove key

--- src/Addon/Lexon/Gmail/src/service/Config.js
+++ src/Addon/Lexon/Gmail/src/service/Config.js
@@ -3,5 +3,5 @@ var urlFrontend = "https://lexbox-test-webaddonlexon.lefebvre.es/";
 var apiEndpoint = "https://lexbox-test-apigwlex.lefebvre.es/api/v1/lex/Lexon/";
 var apiAccount = "https://lexbox-test-apigwacc.lefebvre.es/api/v2/accounts/usermail/";
 var clientId = "871221881170-tt724p6m481ad5kbpa3f0udv4j1q3558.apps.googleusercontent.com";
-var clientSecret = "H-7TJXACoa5JdCCXl_dILSEz";
+var clientSecret = "";
 var urlAuth = "https://lexbox-test-apigwlex.lefebvre.es/api/v1/utils/Lexon/token/login?addTerminatorToToken=true";
