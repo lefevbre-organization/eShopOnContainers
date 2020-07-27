01f06d27679157dffa36a0291412d1d4d17310cd
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-07-27 11:00:51 -0600

    integrating  archive message view and get message raw on office addon

--- src/Addon/Centinela/Office365/src/taskpane/services/services.js
+++ src/Addon/Centinela/Office365/src/taskpane/services/services.js
@@ -23,3 +23,77 @@ export const base64url = (source) => {
 
   return encodedSource;
 }
+
+export const getRawAddon = async (
+  addonData
+) => {
+  const url = `${window.URL_GET_ACCOUNTS}/${addonData.idClienteNav}/raw?`
+  + 'provider='+ addonData.provider + '&account=' + 
+  addonData.account +'&messageId=' + addonData.messageById;
+  console.log(addonData.idClienteNav, addonData.provider, addonData.account, addonData.messageById);
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const result = await response.json();
+
+    return { result };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const saveMessageRaw = async (addonData, raw) => {
+  const url = `${window.URL_GET_ACCOUNTS}/${addonData.idClienteNav}/raw`;
+
+  const body = {
+    id: null,
+    user: addonData.idClienteNav,
+    account: addonData.account,
+    provider: addonData.provider,
+    messageId: addonData.messageById,
+    raw: raw
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/text',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return { response };
+  } catch (err) {
+    throw err;
+  }
+}
+
+export const removeRawAddon = async (addonData) => {
+  const url = `${window.URL_GET_ACCOUNTS}/${addonData.idClienteNav}/raw/delete`;
+  const body = {
+    user: addonData.idClienteNav,
+    account: addonData.account,
+    provider: addonData.provider,
+    messageId: addonData.messageById
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/text',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return { response };
+  } catch (err) {
+    throw err;
+  }
+}
