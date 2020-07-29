290c07c5398df12ba0fa17bd13ee5a657ca2c10b
jvalls-lef <59609845+jvalls-lef@users.noreply.github.com>
2020-07-29 09:49:59 +0200

    Add callbackurl for completed signatures to signature.api

--- src/Services/Signature/Signature.API/Infrastructure/Services/SignaturitService.cs
+++ src/Services/Signature/Signature.API/Infrastructure/Services/SignaturitService.cs
@@ -109,6 +109,7 @@ namespace Signature.API.Infrastructure.Services
             request.AddParameter("branding_id", signatureInfo.brandingId);
             request.AddParameter("reminders", signatureInfo.reminders);
             request.AddParameter("expire_time", signatureInfo.reminders);
+            request.AddParameter("callback_url", _settings.Value.CallBackUrl.ToString());
 
             IRestResponse response = await client.ExecuteAsync(request);
             Console.WriteLine(response.Content);
