26d5d99636fd25a6d8732083fb57c5c228d0d0ba
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2020-08-17 15:34:04 -0600

    new change message viewer on websiganatureclient

--- src/Web/WebSignatureClient/Client/client/src/components/message-viewer/message-viewer.jsx
+++ src/Web/WebSignatureClient/Client/client/src/components/message-viewer/message-viewer.jsx
@@ -71,18 +71,18 @@ export class MessageViewer extends Component {
     //   this.confirmButtonEle = element;
     // };
     this.confirmButtons = [{
-      click: () => {
+        click: () => {
+            this.setState({ hideConfirmDialog: false });
+        },
+        buttonModel: { content: 'No', cssClass: 'btn-modal-close' }
+      }, 
+      {
+        click: () => {
           this.setState({ hideConfirmDialog: false });
           this.onCancelSignatureOk();
-      },
-      buttonModel: { content: 'Si', isPrimary: true }
-      },
-      {
-      click: () => {
-          this.setState({ hideConfirmDialog: false });
-      },
-      buttonModel: { content: 'No', isPrimary: true }
-    }];
+        },
+        buttonModel: { content: 'Si', isPrimary: true }
+      }];
   }
 
   getEventStatus(signer, ev){
@@ -216,19 +216,22 @@ export class MessageViewer extends Component {
     let status_style;
     const contenido = `
     <span class="lf-icon-check-round" style="font-size:100px; padding: 15px;"></span>
-    <div style='text-align: justify; text-justify: inter-word; align-self: center;'>
+    <div style='text-align: justify; text-justify: inter-word; align-self: center;
+    padding-left: 20px;'>
       ${i18n.t('reminderSentModal.text')}
     </div>`;
 
     const contenido2 = `
     <span class="lf-icon-check-round" style="font-size:100px; padding: 15px;"></span>
-    <div style='text-align: justify; text-justify: inter-word; align-self: center;'>
+    <div style='text-align: justify; text-justify: inter-word; align-self: center;
+    padding-left: 20px;'>
       ${i18n.t('cancelledSignatureModal.text')}
     </div>`;
 
     const contenido3 = `
     <span class="lf-icon-question" style="font-size:100px; padding: 15px;"></span>
-    <div style='text-align: justify; text-justify: inter-word; align-self: center;'>
+    <div style='text-align: justify; text-justify: inter-word; align-self: center;
+    padding-left: 20px;'>
       ${i18n.t('cancelConfirmationModal.text2')}
     </div>`;
 
@@ -497,7 +500,7 @@ export class MessageViewer extends Component {
           //header=' ' 
           visible={this.state.hideAlertDialog || this.state.hideAlertDialog2} 
           animationSettings={this.animationSettings} 
-          width='500px' 
+          width='60%' 
           content={(this.state.hideAlertDialog) ? contenido : contenido2}//'Lo sentimos has agotado el número máximo de firmas contratadas. Si lo deseas, puedes contactar con nuestro departamento de atención a cliente en el teléfono 911231231 o pinchando aquí' 
           ref={alertdialog => this.alertDialogInstance = alertdialog} 
           //target='#target' 
@@ -511,9 +514,9 @@ export class MessageViewer extends Component {
           id="confirmDialog" 
           header=' ' 
           visible={this.state.hideConfirmDialog} 
-          //showCloseIcon={true} 
+          showCloseIcon={true} 
           animationSettings={this.animationSettings} 
-          width='400px' 
+          width='60%'
           content={contenido3} 
           ref={dialog => this.confirmDialogInstance = dialog} 
           //target='#target' 
@@ -523,40 +526,117 @@ export class MessageViewer extends Component {
         ></DialogComponent>
         <style global jsx>
           {`
-            #infoDialog, #confirmDialog{
+           #infoDialog, #confirmDialog{
             max-height: 927px;
             width: 300px;
             left: 770px;
             top: 392.5px;
             z-index: 1001;
             transform: translateY(+150%);
-          }
-          #confirmDialog_dialog-header, #confirmDialog_title, #onfirmDialog_dialog-content, .e-footer-content{
-            background: #001970;
-            color: #fff;
-            display:flex;
-          }
-          #infoDialog_dialog-header, #infoDialog_title, #infoDialog_dialog-content, .e-footer-content{
-            background: #001970;
-            color: #fff;
-            display:flex;
-          }
-          #confirmDialog_dialog-header, #confirmDialog_title, #confirmDialog_dialog-content, .e-footer-content{
-            background: #001970;
-            color: #fff;
-            display:flex;
-          }
-          .e-btn.e-flat.e-primary {
-            color: #fff !important;
-          }
-          .e-btn-icon .e-icon-dlg-close .e-icons{
-            color: #fff;
-          }
-          .e-dialog .e-dlg-header-content .e-btn.e-dlg-closeicon-btn{
-            margin-right: 0;
-            margin-left: auto;
-            color: #fff
-          }
+            }
+            #confirmDialog_dialog-header, 
+            #confirmDialog_title, 
+            #confirmDialog_dialog-content, 
+            .e-footer-content{
+              background: #001970;
+              color: #fff;
+              display:flex;
+              width: auto;
+            }
+            #infoDialog_dialog-header, 
+            #infoDialog_title, #infoDialog_dialog-content, 
+            .e-footer-content{
+              background: #001970;
+              color: #fff;
+              display:flex;
+            }
+            #confirmDialog_dialog-header, 
+            #confirmDialog_title, 
+            #confirmDialog_dialog-content, 
+            .e-footer-content{
+              background: #001970;
+              color: #fff;
+              display:flex;
+            }
+            .e-btn.e-flat.e-primary {
+              color: #fff !important;
+            }
+            .e-btn-icon .e-icon-dlg-close .e-icons{
+              color: #fff;
+            }
+            .e-dialog .e-dlg-header-content .e-btn.e-dlg-closeicon-btn{
+              margin-right: 0;
+              margin-left: auto;
+              color: #fff
+            } 
+            .e-dialog .e-dlg-header-content 
+            .e-btn.e-dlg-closeicon-btn {
+                margin-right: 0;
+                margin-left: auto;
+                color: #fff;
+                height: 15px;
+                background-color: transparent;
+            } 
+            #confirmDialog_dialog-header, .e-dialog 
+            .e-icon-dlg-close::before {
+              content: '\e7fc';
+              position: relative;
+              color: white;
+              font-size: 15px;
+            }
+            #confirmDialog .e-btn.e-flat.e-primary {
+              text-transform: uppercase;
+              font-size: 13px;
+              font-family: MTTMilano-Bold,Lato,Arial,sans-serif;
+              letter-spacing: .7px;
+              color: #001978 !important;
+              padding: 10px;
+              background-color: #fff;
+              border-radius: 0 !important;
+              border: 2px solid #fff !important;
+              min-width: 80px;
+            }
+            
+            #confirmDialog .e-btn.e-flat.e-primary:hover {
+              background-color: #e5e8f1 !important;
+              background: #e5e8f1 !important;
+              color: #001978 !important;
+            }
+            
+            #confirmDialog .e-btn.e-flat.e-primary:active {
+              background-color: #e5e8f1 !important;
+              background: #e5e8f1 !important;
+              color: #001978 !important;
+            }
+  
+            .btn-modal-close {
+              text-transform: uppercase;
+              font-size: 13px;
+              font-family: MTTMilano-Bold,Lato,Arial,sans-serif;
+              letter-spacing: .7px;
+              color: #fff !important;
+              padding: 10px;
+              background-color: #001978 !important;
+              min-width: 80px;
+              border-radius: 0 !important;
+              border: 2px solid #fff !important;
+            }
+            
+            .btn-modal-close:hover {
+              background-color: #e5e8f1 !important;
+              background: #e5e8f1 !important;
+              color: #001978 !important;
+            }
+            
+            .btn-modal-close:active {
+              background-color: #e5e8f1 !important;
+              background: #e5e8f1 !important;
+              color: #001978 !important;
+            }
+
+            #infoDialog, .e-dialog .e-dlg-content {
+              font-size: 17.5px !important;
+            }
           
           `}
         </style>
