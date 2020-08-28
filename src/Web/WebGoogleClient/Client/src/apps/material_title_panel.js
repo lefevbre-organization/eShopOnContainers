39876958369ee660722d1f6202175dd893ff6d1a
avalverde-lef <a.valverde-ext@lefebvre.es>
2020-08-28 13:02:06 +0200

    AB#1113 fixin bug of remove calendar expand icon in connector

--- src/Web/WebGoogleClient/Client/src/apps/material_title_panel.js
+++ src/Web/WebGoogleClient/Client/src/apps/material_title_panel.js
@@ -41,7 +41,8 @@ const MaterialTitlePanel = props => {
           src="/assets/img/close.png"
           onClick={() => _handleOnClick()}
         ></img>
-                <Link to="chart" target="_blank" to="calendar" >
+
+              {/*<Link to="chart" target="_blank" to="calendar" >
               <img
                       className="headerButtons"
                       alt={props.title}
@@ -49,7 +50,7 @@ const MaterialTitlePanel = props => {
                       src="/assets/img/expand.png"
 
                   ></img>
-             </Link>
+             </Link>*/}
         
       </div>
       {props.children}
