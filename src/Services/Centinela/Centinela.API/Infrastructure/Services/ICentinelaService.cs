b0360e07136bd0594bda873a7fc19f2b7d680446
jvalls-lef <59609845+jvalls-lef@users.noreply.github.com>
2020-11-20 13:55:12 +0100

    Add new method to notify Centinela a new signature request has been created correctly
    AB#1411

--- src/Services/Centinela/Centinela.API/Infrastructure/Services/ICentinelaService.cs
+++ src/Services/Centinela/Centinela.API/Infrastructure/Services/ICentinelaService.cs
@@ -27,5 +27,7 @@ namespace Lefebvre.eLefebvreOnContainers.Services.Centinela.API.Infrastructure.S
 
         Task<Result<bool>> CancelSignatureAsync(string guid);
 
+        Task<Result<bool>> NotifySignatureAsync(string service, string guid, string documentId, List<CenRecipient> recipients);
+
     }
 }
