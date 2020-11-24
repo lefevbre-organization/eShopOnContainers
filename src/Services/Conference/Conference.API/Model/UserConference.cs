4202b5599831d68d1931ef4e0ae678fd515e5f73
freyes-lef <f.reyes-ext@lefebvre.es>
2020-11-24 18:49:47 +0100

    refactoring models conference

--- src/Services/Conference/Conference.API/Model/UserConference.cs
+++ src/Services/Conference/Conference.API/Model/UserConference.cs
@@ -9,17 +9,4 @@
         public ConferenceModel[] conferences { get; set; }
         public ConferenceStats[] stats { get; set; }
     }
-
-    public class UserReservation : UserReservationRequest
-    {
-        public long id { get; set; } //': 364758328,
-        public long duration { get; set; } //  'duration': 900000
-    }
-
-    public class UserReservationRequest
-    {
-        public string name { get; set; }
-        public string mail_owner { get; set; }
-        public string start_time { get; set; } //  'start_time': '2048-04-20T17:55:12.000Z',
-    }
 }
---
+++ src/Services/Conference/Conference.API/Model/UserReservationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Lefebvre.eLefebvreOnContainers.Services.Conference.API.Models
+{
+    public class UserReservationRequest
+    {
+        public string name { get; set; }
+        public string mail_owner { get; set; }
+        public string start_time { get; set; } //  'start_time': '2048-04-20T17:55:12.000Z',
+    }
+}
