f66eb2f1fcc6ef2a6f42c355c3b2399e945e5e9a
freyes-lef <f.reyes-ext@lefebvre.es>
2020-11-26 12:16:09 +0100

    update values charts helm version 36

--- deploy/k8s-lef/helm/deploy-all.ps1
+++ deploy/k8s-lef/helm/deploy-all.ps1
@@ -7,7 +7,7 @@ Param(
     [parameter(Mandatory=$false)][bool]$clean=$true,
     [parameter(Mandatory=$false)][bool]$deployInfrastructure=$true,
     [parameter(Mandatory=$false)][string[]]$infras=(
-        # "rabbitmq",
+        "rabbitmq",
         # "consul","vault",
         "nosql-data",
         "sql-data"
