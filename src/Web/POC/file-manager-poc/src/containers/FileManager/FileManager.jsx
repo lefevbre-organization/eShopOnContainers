065cc186e1ec25e3826bc706a312ff8d6604f60e
jlopezdetrinidad <j.hostilio-ext@lefebvre.es>
2021-01-25 08:10:12 -0600

    Adding DragAndDrop

--- src/Web/POC/file-manager-poc/src/containers/FileManager/FileManager.jsx
+++ src/Web/POC/file-manager-poc/src/containers/FileManager/FileManager.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
 	FileManagerComponent, 
 	Inject, 
@@ -11,19 +11,50 @@ import { Row, Col } from 'reactstrap';
 import './FileManager.scss';
 
 const FileManager = () => {
+	const [navigatorLanguage, setLanguage] = useState('');
 	const hostUrl = 'https://ej2-aspcore-service.azurewebsites.net/';
+
+	useEffect(() => {
+		const languageSpit = (navigator.language).split('-');
+		setLanguage(languageSpit[0]);
+		console.log(navigatorLanguage)
+	}, [])
+
+	// File Manager Drag start event
+	const onFileDragStart = (args) => {
+		console.log('File Drag Start', args);
+	}
+	// File Manager Drag start event
+	const onFileDragStop = (args) => {
+		console.log('File Drag Stop', args);
+	}
+	// File Manager Drag start event
+	const onFileDragging = (args) => {
+		console.log('File Dragging', args);
+	}
+	// File Manager Drag start event
+	const onFileDropped = (args) => {
+		console.log('File Dropped', args);
+	}
+	
 	return (
 		<Row className="main-file-manager">
 			<Col>
 				<FileManagerComponent 
 					id="overview_file" 
+					locale={navigatorLanguage}
+					view={'Details'} 
+					allowDragAndDrop={true} 
 					ajaxSettings={{
 						url: hostUrl + 'api/FileManager/FileOperations',
 						getImageUrl: hostUrl + 'api/FileManager/GetImage',
 						uploadUrl: hostUrl + 'api/FileManager/Upload',
 						downloadUrl: hostUrl + 'api/FileManager/Download'
 					}} 
-					view={'Details'} >
+					fileDragStart={onFileDragStart} 
+					fileDragStop={onFileDragStop} 
+					fileDragging={onFileDragging} 
+					fileDropped={onFileDropped} >
 					<Inject services={[
 						NavigationPane, 
 						DetailsView, 
