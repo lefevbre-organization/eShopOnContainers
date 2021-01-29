bdca1280bf459011a762a5e22f7dda5c9452ed39
freyes-lef <f.reyes-ext@lefebvre.es>
2021-01-29 15:34:33 +0100

    fix dockerfiles and vaues of chart AB#1631

--- deploy/k8s-lef/build-lefebvre.ps1
+++ deploy/k8s-lef/build-lefebvre.ps1
@@ -7,7 +7,7 @@ Param(
     [parameter(Mandatory=$false)][bool]$buildImages=$true,
     [parameter(Mandatory=$false)][bool]$buildAll=$false,
     [parameter(Mandatory=$false)][string[]]$servicesToBuild=(
-         "calendar.api", "account.api"
+         "calendar.api"
         # "calendar.api", "conference.api", "account.api", "lexon.api", "centinela.api", "userutils.api", "signature.api", #  "database.api", 
         # "googledrive.api", "googleaccount.api",        
         # "webportalclient", "webgoogleclient", "webofficeclient", "weblexonclient", "webimapclient", "websignatureclient", "webcentinelaclient", # "webdatabaseclient", 
@@ -17,7 +17,7 @@ Param(
          ),  
     [parameter(Mandatory=$false)][bool]$pushImages=$true,
     [parameter(Mandatory=$false)][string[]]$servicesToPush=(
-        "calendar.api", "account.api"
+        "calendar.api"
         # "calendar.api","conference.api", "account.api", "lexon.api", "centinela.api", "userutils.api", "signature.api",        #  "database.api",
         # "googledrive.api", "googleaccount.api",
         # "webportalclient", "webgoogleclient", "webofficeclient", "weblexonclient", "webimapclient", "websignatureclient", "webcentinelaclient",         # "webdatabaseclient", 
@@ -25,7 +25,7 @@ Param(
         # "ocelotapigw", 
         # "webstatuslef"
         ),
-    [parameter(Mandatory=$false)][string]$imageEnv="dev-39.7",
+    [parameter(Mandatory=$false)][string]$imageEnv="dev-39.3",
     [parameter(Mandatory=$false)][string]$imagePlatform="linux",
     [parameter(Mandatory=$false)][bool]$deployCI=$false
 )
