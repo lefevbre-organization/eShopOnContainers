50ac49d717bf246973af30027e1d78ce37c3c150
freyes-lef <f.reyes-ext@lefebvre.es>
2021-02-01 10:32:08 +0100

    increase fields of LexAppointment

--- src/Services/Lexon/Lexon.API/Model/LexAppointment.cs
+++ src/Services/Lexon/Lexon.API/Model/LexAppointment.cs
@@ -5,13 +5,16 @@
         public int? Id { get; set; }
 
         public string Subject { get; set; }
+        public string Notes { get; set; }
+        public short? AllDay { get; set; }
         public string IdEvent { get; set; }
         public string Provider { get; set; }
         public string Location { get; set; }
+        public string Calendar { get; set; }
+        public string Assistants { get; set; }
         public string EndDate { get; set; }
         public string StartDate { get; set; }
-
-        //'{"BBDD":"lexon_admin_02","IdUser":1344, "Subject":"test cita", "Location":"Madrid", "EndDate":"2020-03-30 20:31:30", "StartDate":"2020-03-28 20:31:30"}';
+       
     }
 
 
