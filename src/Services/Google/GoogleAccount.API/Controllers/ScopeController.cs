424f36d74947c9ebe65cf5ea24005ed3b53ac49b
freyes-lef <f.reyes-ext@lefebvre.es>
2021-02-08 19:25:04 +0100

    fix controller on scopes

--- src/Services/Google/GoogleAccount.API/Controllers/ScopeController.cs
+++ src/Services/Google/GoogleAccount.API/Controllers/ScopeController.cs
@@ -8,20 +8,27 @@ namespace Lefebvre.eLefebvreOnContainers.Services.Google.Account.API.Controllers
 
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.eShopOnContainers.BuildingBlocks.Lefebvre.Models;
-    using Lefebvre.eLefebvreOnContainers.Services.Google.Account.API.Infrastructure.Services;
+    using Infrastructure.Services;
     using Model;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.AspNetCore.Http;
 
-    [ApiController]
     [Route("api/v1/[controller]")]
-    public class ScopeController: Controller
+    [ApiController]
+    public class ScopeController: ControllerBase
     {
 
         private readonly IScopeService _service;
+        internal readonly ILogger<ScopeController> _log;
+
 
         public ScopeController(
             IScopeService service
+            , ILogger<ScopeController> log
+
             )
         {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
             _service = service ?? throw new ArgumentNullException(nameof(service));
         }
 
@@ -33,23 +40,35 @@ namespace Lefebvre.eLefebvreOnContainers.Services.Google.Account.API.Controllers
             return Ok(new Result<bool>(true));
         }
 
-        [HttpGet("[action]")]
+        [HttpGet("/{product}")]
         [ProducesResponseType(typeof(Result<List<GoogleAccountScope>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Result<List<GoogleAccountScope>>), (int)HttpStatusCode.BadRequest)]
-        public async Task<ActionResult> GetAllForProduct([FromQuery] GoogleProduct? product)
+        public async Task<IActionResult> GetAllForProduct([FromRoute] GoogleProduct product = GoogleProduct.Drive  )
         {
 
-            if (product == null)
-                return BadRequest("Debe tener un valor válido para Producto (0 Drive).");
+            //if (product == null)
+            //    return BadRequest("Debe tener un valor válido para Producto (0 Drive).");
 
-            return Ok(await _service.GetScopes(product.Value));
+            //return Ok(await _service.GetScopes(product));
+            var result = await _service.GetScopes(product);
+            if (result.errors?.Count > 0 && result.data == null)
+            {
+                result.data = null;
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+            else if (result.errors?.Count == 0 && result.data.Count == 0)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
         }
 
         [HttpPost]
-        [Route("[action]")]
         [ProducesResponseType(typeof(Result<GoogleAccountScope>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Result<GoogleAccountScope>), (int)HttpStatusCode.BadRequest)]
-        public async Task<ActionResult> CreateScope([FromBody] GoogleAccountScope scope)
+        public async Task<IActionResult> CreateScope(
+            [FromBody] GoogleAccountScope scope)
         {
             if (scope == null)
                 return BadRequest("Necesita ingresar un modelo válido");
@@ -57,19 +76,23 @@ namespace Lefebvre.eLefebvreOnContainers.Services.Google.Account.API.Controllers
             if (string.IsNullOrEmpty(scope.Url))
                 return BadRequest("Un Scope necesita como mínimo una url válida");
 
-            return Ok(await _service.CreateScope(scope));
+            var result = await _service.CreateScope(scope);
+            return result.errors?.Count > 0 ? (IActionResult)BadRequest(result) : Ok(result);
+        
         }
 
-        [HttpDelete("[action]")]
+        [HttpDelete]
         [ProducesResponseType(typeof(Result<bool>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Result<bool>), (int)HttpStatusCode.BadRequest)]
-        public async Task<ActionResult> DeleteScope([FromQuery] string ScopeId)
+        public async Task<IActionResult> DeleteScope([FromQuery] string ScopeId)
         {
 
             if (string.IsNullOrEmpty(ScopeId))
                 return BadRequest("Necesita un Id válido para eliminar su Scope");
 
-            return Ok(await _service.DeleteScope(ScopeId));
+            //return Ok(await _service.DeleteScope(ScopeId));
+            var result = await _service.DeleteScope(ScopeId);
+            return result.errors?.Count > 0 ? (IActionResult)BadRequest(result) : Ok(result);
         }
 
 
