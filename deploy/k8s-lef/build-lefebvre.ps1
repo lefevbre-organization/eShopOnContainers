c60e5e42890c58477186fd2bc4927fd6459d09ef
freyes-lef <f.reyes-ext@lefebvre.es>
2021-03-12 13:53:59 +0100

    fix error in pipelines

--- deploy/k8s-lef/build-lefebvre.ps1
+++ deploy/k8s-lef/build-lefebvre.ps1
@@ -7,7 +7,7 @@ Param(
     [parameter(Mandatory=$false)][bool]$buildImages=$true,
     [parameter(Mandatory=$false)][bool]$buildAll=$false,
     [parameter(Mandatory=$false)][string[]]$servicesToBuild=(
-        "webgoogleclient"
+        "webofficeclient"
         # "calendar.api", "conference.api", "account.api", "lexon.api", "centinela.api", "userutils.api", "signature.api",  "database.api", "googleaccount.api", "googledrive.api"        
         # "webportalclient", "webgoogleclient", "webofficeclient", "weblexonclient", "webimapclient", "websignatureclient", "webcentinelaclient", "webdatabaseclient" ,
         # "webaddonlauncher", "weboffice365addonlexon", "weboffice365addoncentinela" 
@@ -16,7 +16,7 @@ Param(
          ),  
     [parameter(Mandatory=$false)][bool]$pushImages=$true,
     [parameter(Mandatory=$false)][string[]]$servicesToPush=(
-        "webgoogleclient"
+        "webofficeclient"
         # "calendar.api","conference.api", "account.api", "lexon.api", "centinela.api", "userutils.api", "signature.api", "database.api", "googleaccount.api", "googledrive.api"
         # "webportalclient", "webgoogleclient", "webofficeclient", "weblexonclient", "webimapclient", "websignatureclient", "webcentinelaclient", "webdatabaseclient",
         # "webaddonlauncher", "weboffice365addonlexon", "weboffice365addoncentinela"
